b04449e04e4442cc3e4af5598cca17e7ad0a33d4
Rachit Gupta <rachit.gupta@jabong.com>
2015-06-18 14:02:16 +0530

    Formatted classes.

--- core/src/main/scala/com/jabong/dap/data/acq/common/Utils.scala
+++ core/src/main/scala/com/jabong/dap/data/acq/common/Utils.scala
@@ -5,17 +5,53 @@ import java.sql.{ DriverManager, Connection }
 import com.jabong.dap.common.json.EmptyClass
 
 /**
- * Created by Rachit on 10/6/15.
+ * Case class for storing information about join tables.
+ *
+ * @param name String The name of the table.
+ * @param foreignKey String The name of the foreign key for the table.
  */
+case class JoinTables(
+  name: String,
+  foreignKey: String
+)
 
-// Classes for storing the JSON schema.
-case class JoinTables(name: String, foreignKey: String)
+/**
+ * Case class for storing information about data acquisition from a table.
+ *
+ * @param source String The source of the data. (Can be erp, bob, unicommerce or nextbee)
+ * @param tableName String The name of the table.
+ * @param primaryKey String The primary key of the table.
+ * @param mode String The mode of the data acquisition. (Can be full, daily or hourly)
+ * @param saveFormat String The format in which the data is to be saved. (Can be orc or parquet)
+ * @param saveMode String The mode in which the data is to be saved. (Can be overwrite, append, error or ignore)
+ * @param dateColumn String The name of the column which represents the date time when the row was updated.
+ * @param rangeStart String The date time from which the data is to be fetched.
+ * @param rangeEnd String The date time till which the data is to be fetched.
+ * @param limit String The number of rows to be fetched.
+ * @param joinTables List[JoinTables] List of tables to be joined.
+ */
+case class TableInfo(
+  source: String,
+  tableName: String,
+  primaryKey: String,
+  mode: String,
+  saveFormat: String,
+  saveMode: String,
+  dateColumn: String,
+  rangeStart: String,
+  rangeEnd: String,
+  limit: String,
+  joinTables: List[JoinTables]
+)
 
-case class TableInfo(source: String, tableName: String, primaryKey: String, mode: String, saveFormat: String,
-                     saveMode: String, dateColumn: String, rangeStart: String, rangeEnd: String, limit: String,
-                     joinTables: List[JoinTables])
-
-case class ImportInfo(acquisition: List[TableInfo]) extends EmptyClass
+/**
+ * Case class for storing the information for the data acquisition.
+ *
+ * @param acquisition List[TableInfo] List of tables to acquire the data from.
+ */
+case class ImportInfo(
+  acquisition: List[TableInfo]
+) extends EmptyClass
 
 object DaoUtil {
 
