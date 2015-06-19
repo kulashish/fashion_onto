97154415e1b2def31e20ebe270841e9fdb6495a8
Rachit Gupta <rachit.gupta@jabong.com>
2015-06-19 19:40:04 +0530

    Added more time unit test cases.

--- core/src/test/scala/com/jabong/dap/common/utils/TimeTest.scala
+++ core/src/test/scala/com/jabong/dap/common/utils/TimeTest.scala
@@ -3,8 +3,9 @@ package com.jabong.dap.common.utils
 import java.text.SimpleDateFormat
 
 import com.jabong.dap.common.Constants
+import com.jabong.dap.common.utils.Time.MonthYear
 import org.scalatest.{Matchers, FlatSpec}
-import java.util.Date
+import java.util.{Calendar, Date}
 
 /**
  * Created by Rachit on 19/6/15.
@@ -22,5 +23,70 @@ class TimeTest extends FlatSpec with Matchers {
 
   "daysFromToday" should "return " + numDaysFromToday in {
     Time.daysFromToday(date2) should be (numDaysFromToday)
+  }
+
+  "dateBefore30Days" should "return correct value" in {
+    val cal = Calendar.getInstance()
+    cal.add(Calendar.DAY_OF_MONTH, -30)
+    Time.dateBefore30Days() should be (cal.getTime)
+  }
+
+  "getTodayDate" should "return correct value" in {
+    val sdf = new SimpleDateFormat(Constants.DateFormat)
+    Time.getTodayDate() should be (sdf.format(today))
+  }
+
+  "getYesterdayDate" should "return correct value" in {
+    val cal = Calendar.getInstance()
+    val sdf = new SimpleDateFormat(Constants.DateFormat)
+    cal.setTime(today)
+    cal.add(Calendar.DAY_OF_MONTH, -1)
+    Time.getYesterdayDate() should be (sdf.format(cal.getTime))
+  }
+
+  "getTodayDateWithHrs" should "return correct value" in {
+    val sdf = new SimpleDateFormat("yyyy-MM-dd-HH")
+    Time.getTodayDateWithHrs() should be (sdf.format(today))
+  }
+
+  "getMonthAndYear" should "return correct value" in {
+    val my = new MonthYear(6, 2015, 19)
+    Time.getMonthAndYear("2015-06-19") should be (my)
+  }
+
+  "getMaxDaysOfMonth" should "return 30" in {
+    Time.getMaxDaysOfMonth("2015-06-19") should be (30)
+  }
+
+  "dateStringEmpty" should "return true" in {
+    Time.dateStringEmpty("") should be (true)
+  }
+
+  "dateStringEmpty" should "return false" in {
+    Time.dateStringEmpty("2015-06-19") should be (false)
+  }
+
+  "isStrictlyLessThan" should "return true" in {
+    Time.isStrictlyLessThan("2015-05-19 00:00:00", "2015-06-19 00:00:00") should be (true)
+  }
+
+  "isStrictlyLessThan" should "return false" in {
+    Time.isStrictlyLessThan("2015-07-19 00:00:00", "2015-06-19 00:00:00") should be (false)
+  }
+
+  "isSameMonth" should "return true" in {
+    Time.isSameMonth("2015-06-19 00:00:00", "2015-06-20 00:00:00") should be (true)
+  }
+
+  "isSameMonth" should "return false" in {
+    Time.isSameMonth("2015-06-19 00:00:00", "2015-05-19 00:00:00") should be (false)
+  }
+
+  "isSameDay" should "return true" in {
+    Time.isSameDay("2015-06-19 00:00:00", "2015-06-19 00:00:00") should be (true)
+  }
+
+  "isSameDay" should "return false" in {
+    Time.isSameDay("2015-06-19 00:00:00", "2015-05-19 00:00:00") should be (false)
   }
 }
