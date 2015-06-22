a1dfab4adcc226a59f8aff9be58c84460127417d
abhayjabong <abhay.gupta@jabong.com>
2015-06-22 13:16:17 +0530

    Path change.

--- core/src/test/scala/com/jabong/dap/data/acq/QueryBuilderTest.scala
+++
@@ -1,8 +0,0 @@
-package com.jabong.dap.data.acq
-
-/**
- * Created by test on 22/6/15.
- */
-class QueryBuilderTest {
-
-}
---
+++ core/src/test/scala/com/jabong/dap/data/acq/common/QueryBuilderTest.scala
@@ -0,0 +1,8 @@
+package com.jabong.dap.data.acq.common
+
+/**
+ * Created by Abhay on 22/6/15.
+ */
+class QueryBuilderTest {
+
+}
