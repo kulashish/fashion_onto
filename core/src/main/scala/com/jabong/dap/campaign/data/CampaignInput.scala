5f0b50c1574f53943c2290cc4987445203658d5c
Rahul Aneja <rahul.aneja@jabong.com>
2015-07-01 11:12:06 +0530

    added data and utils for campaigns

---
+++ core/src/main/scala/com/jabong/dap/campaign/data/CampaignInput.scala
@@ -0,0 +1,20 @@
+package com.jabong.dap.campaign.data
+
+import org.apache.spark.sql.DataFrame
+
+/**
+ * Created by jabong1145 on 15/6/15.
+ */
+class CampaignInput {
+
+  def loadCustomerData(): DataFrame ={
+    return null
+  }
+
+  def loadProductData(): DataFrame = {
+    return null
+  }
+
+
+
+}
---
+++ core/src/main/scala/com/jabong/dap/campaign/utils/CampaignUtils.scala
@@ -0,0 +1,9 @@
+package com.jabong.dap.campaign.utils
+
+/**
+ * Created by jabong1145 on 15/6/15.
+ */
+class CampaignUtils {
+
+
+}
