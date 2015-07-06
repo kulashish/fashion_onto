991cb21834f4c5bfcb13960968717cb6fda6b994
jabong <jabong@jabong11.(none)>
2015-07-06 14:57:33 +0530

    refactoring code

--- core/src/main/scala/com/jabong/dap/model/order/variables/SalesRule.scala
+++ core/src/main/scala/com/jabong/dap/model/order/variables/SalesRule.scala
@@ -1,7 +1,8 @@
 package com.jabong.dap.model.order.variables
 
 import com.jabong.dap.common.constants.variables.SalesRuleVariables
-import com.jabong.dap.common.{ Spark, MergeUtils}
+import com.jabong.dap.common.Spark
+import com.jabong.dap.common.merge.MergeUtils
 import com.jabong.dap.data.storage.DataSets
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
