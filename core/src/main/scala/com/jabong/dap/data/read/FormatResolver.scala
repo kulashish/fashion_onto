5a3135f3c0cd99e5c3c942628a0e8e88eb4911ba
abhayjabong <abhay.gupta@jabong.com>
2015-07-16 18:17:33 +0530

    Added functionality to check for format automatically (orc or parquet).

---
+++ core/src/main/scala/com/jabong/dap/data/read/FormatResolver.scala
@@ -0,0 +1,49 @@
+package com.jabong.dap.data.read
+
+import java.io.File
+import scala.util.control.Breaks._
+
+/**
+ * Object to get check whether the format of data stored at a given directory
+ * is orc or parquet.
+ * WARNING: raises ValidFormatNotFound Exception in case orc or parquet file
+ * is not found in the given directory.
+ */
+object FormatResolver {
+
+  def getFormat(directory: String): String = {
+    val dir = new File(directory)
+    val directoryListing = dir.listFiles()
+    var flag: String = null
+    if (directoryListing != null) {
+      breakable {
+        for (file <- directoryListing) {
+          val extension = getFileExtension(file)
+          if (extension == "orc" || extension == "parquet") {
+            flag = extension
+            break()
+          }
+        }
+      }
+    } else {
+      throw new ValidFormatNotFound
+    }
+    if (flag != null) {
+      flag
+    } else {
+      throw new ValidFormatNotFound
+    }
+  }
+
+
+  def getFileExtension(file: File): String = {
+    val fileName = file.getName
+    if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0) {
+      fileName.substring(fileName.lastIndexOf(".") + 1)
+    } else {
+      null
+    }
+  }
+}
+
+class ValidFormatNotFound extends Exception
--- core/src/main/scala/com/jabong/dap/data/read/RawDataRead.scala
+++ core/src/main/scala/com/jabong/dap/data/read/RawDataRead.scala
@@ -9,18 +9,22 @@ object RawDataRead extends Logging {
   /**
    * Method to read raw HDFS data for a source and table and get a dataFrame for the same.
    * WARNING: Throws DataNotFound exception if data is not found.
+   * WARNING: Throws ValidFormatNotFound exception if suitable format is not found.
    */
-  def getDataFrame (source: String, tableName: String, dataType: String, date: String, saveFormat: String): DataFrame = {
+  def getDataFrame (source: String, tableName: String, dataType: String, date: String): DataFrame = {
     require(source != null, "Source Type is null")
     require(tableName != null, "Table Name is null")
     require(dataType != null, "Data Type is null")
-    require(saveFormat != null, "Save Format is null")
+
+
 
 
     val pathDate = DateResolver.resolveDate(date, dataType)
     try {
       val dateHour = DateResolver.getDateHour(source, tableName, dataType, pathDate)
       val pathDateWithHour = "%s-%s".format(pathDate, dateHour)
+
+      val saveFormat = FormatResolver.getFormat(pathDateWithHour)
 
       val fetchPath = PathBuilder.buildPath(source, tableName, dataType, pathDateWithHour)
       val context = getContext(saveFormat)
@@ -29,6 +33,9 @@ object RawDataRead extends Logging {
       case e : DataNotFound =>
         logger.error("Data not found for the date")
         throw new DataNotFound
+      case e : ValidFormatNotFound =>
+        logger.error("Format could not be resolved for the given files in directory")
+        throw new ValidFormatNotFound
     }
   }
 
