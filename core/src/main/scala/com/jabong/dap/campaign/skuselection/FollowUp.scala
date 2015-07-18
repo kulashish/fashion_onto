f6a073b96516b2dc99f21c5c8b3b71fea2509d88
Rahul Aneja <rahul.aneja@jabong.com>
2015-07-18 21:09:52 +0530

    follow up changes

--- core/src/main/scala/com/jabong/dap/campaign/skuselection/FollowUp.scala
+++ core/src/main/scala/com/jabong/dap/campaign/skuselection/FollowUp.scala
@@ -3,9 +3,10 @@ package com.jabong.dap.campaign.skuselection
 import com.jabong.dap.campaign.utils.CampaignUtils
 import com.jabong.dap.common.constants.campaign.CampaignCommon
 import com.jabong.dap.common.constants.variables.{CustomerVariables, ProductVariables}
+import grizzled.slf4j.Logging
 import org.apache.spark.sql.DataFrame
 
-class FollowUp extends SkuSelector {
+class FollowUp extends SkuSelector with Logging {
 
   // 1. order should not have been placed for the ref sku yet
   // 2. pick based on special price (descending)
@@ -14,6 +15,7 @@ class FollowUp extends SkuSelector {
   // itrData = [(skusimple, date, special price)]
   override def skuFilter(customerSkuData: DataFrame, itrData: DataFrame): DataFrame = {
     if(customerSkuData == null || itrData==null){
+      logger.error("either customer selected skus are null or itrData is null",throw new IllegalArgumentException)
       return null
     }
 
@@ -23,7 +25,10 @@ class FollowUp extends SkuSelector {
         ,customerSkuData(ProductVariables.SKU)
         ,customerSkuData(ProductVariables.SPECIAL_PRICE))
 
+    logger.info("Join selected customer sku with sku data and filter by stock>="+CampaignCommon.FOLLOW_UP_STOCK_VALUE)
+    //generate reference skus
     val refSkus =  CampaignUtils.generateReferenceSkus(filteredSku,CampaignCommon.NUMBER_REF_SKUS)
+
     return refSkus
   }
 
