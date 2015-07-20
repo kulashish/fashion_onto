914b1259caa83ff7faa2b39b43933c699039716c
apoorvamoghey <apoorva.moghey@jabong.com>
2015-07-20 16:39:54 +0530

    change column name

--- core/src/main/scala/com/jabong/dap/model/product/itr/ERP.scala
+++ core/src/main/scala/com/jabong/dap/model/product/itr/ERP.scala
@@ -1,5 +1,6 @@
 package com.jabong.dap.model.product.itr
 
+import com.jabong.dap.model.product.itr.variables.ITR
 import org.apache.spark.sql.DataFrame
 
 object ERP {
@@ -44,20 +45,20 @@ object ERP {
       "segment",
       "businessunit"
     ).
-      withColumnRenamed("No_", "barcodeEan").
-      withColumnRenamed("vendoritemno_", "vendorItemNo").
-      withColumnRenamed("heelheight", "heelHeight").
-      withColumnRenamed("suppliercolor", "supplierColor").
-      withColumnRenamed("description", "styleName").
-      withColumnRenamed("suppstylecode", "supplierStyleCode").
-      withColumnRenamed("dispatchlocation", "dispatchLocation").
-      withColumnRenamed("reportingsubcategory", "reportingSubcategory").
-      withColumnRenamed("reportingcategory", "reportingCategory").
-      withColumnRenamed("brandtype", "brandType").
-      withColumnRenamed("itemtype", "itemType").
-      withColumnRenamed("mrpprice", "mrpPrice").
-      withColumnRenamed("no_2", "jabongCode").
-      withColumnRenamed("businessunit", "businessUnit").
-      withColumnRenamed("petstylecode", "petStyleCode")
+      withColumnRenamed("No_", ITR.BARCODE_EAN).
+      withColumnRenamed("vendoritemno_", ITR.VENDOR_ITEM_NO).
+      withColumnRenamed("heelheight", ITR.HEEL_HEIGHT).
+      withColumnRenamed("suppliercolor", ITR.SUPPLIER_COLOR).
+      withColumnRenamed("description", ITR.STYLE_NAME).
+      withColumnRenamed("suppstylecode", ITR.SUPPLIER_STYLE_CODE).
+      withColumnRenamed("dispatchlocation", ITR.DISPATCH_LOCATION).
+      withColumnRenamed("reportingsubcategory", ITR.REPORTING_SUBCATEGORY).
+      withColumnRenamed("reportingcategory", ITR.REPORTING_CATEGORY).
+      withColumnRenamed("brandtype", ITR.BRAND_TYPE).
+      withColumnRenamed("itemtype", ITR.ITEM_TYPE).
+      withColumnRenamed("mrpprice", ITR.MRP_PRICE).
+      withColumnRenamed("No_", ITR.JABONG_CODE).
+      withColumnRenamed("businessunit", ITR.BUSINESS_UNIT).
+      withColumnRenamed("petstylecode", ITR.PET_STYLE_CODE)
   }
 }
