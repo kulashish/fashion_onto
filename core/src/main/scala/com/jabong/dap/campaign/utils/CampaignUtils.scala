cdf36119f22fdd7fc0940f8be13a455838965c34
Rahul Aneja <rahul.aneja@jabong.com>
2015-07-20 17:40:46 +0530

    refactoring

--- core/src/main/scala/com/jabong/dap/campaign/utils/CampaignUtils.scala
+++ core/src/main/scala/com/jabong/dap/campaign/utils/CampaignUtils.scala
@@ -111,9 +111,9 @@ object CampaignUtils extends Logging {
   def startOfDay(time: Date): Long = {
     val cal = Calendar.getInstance();
     cal.setTimeInMillis(time.getTime());
-    cal.set(Calendar.HOUR_OF_DAY,0); //set hours to 0
-    cal.set(Calendar.MINUTE,0); // set minutes to 0
-    cal.set(Calendar.SECOND,0); //set seconds to 0
+    cal.set(Calendar.HOUR_OF_DAY, 0); //set hours to 0
+    cal.set(Calendar.MINUTE, 0); // set minutes to 0
+    cal.set(Calendar.SECOND, 0); //set seconds to 0
     return cal.getTime.getTime
   }
 
@@ -187,25 +187,25 @@ object CampaignUtils extends Logging {
    * @param before
    * @return
    */
-  def getTimeBasedDataFrame(inData: DataFrame,timeField:String , after: String, before: String): DataFrame = {
-    if(inData == null || timeField == null || before == null || after ==null) {
+  def getTimeBasedDataFrame(inData: DataFrame, timeField: String, after: String, before: String): DataFrame = {
+    if (inData == null || timeField == null || before == null || after == null) {
       logger.error("Any of the value in getTimeBasedDataFrame is null")
       return null
     }
 
-    if(after.length != before.length) {
+    if (after.length != before.length) {
       logger.error("before and after time formats are different ")
       return null
     }
 
     val Columns = inData.columns
-     if(!(Columns contains(timeField))){
-       logger.error(timeField+"doesn't exist in the inData Frame Schema")
-       return null
-     }
+    if (!(Columns contains (timeField))) {
+      logger.error(timeField + "doesn't exist in the inData Frame Schema")
+      return null
+    }
 
-    val filteredData = inData.filter(timeField+" >= '"+after+"' and "+ timeField +" <= '"+before+"'")
-    logger.info("Input Data Frame has been filtered before"+before + "after '"+after)
+    val filteredData = inData.filter(timeField + " >= '" + after + "' and " + timeField + " <= '" + before + "'")
+    logger.info("Input Data Frame has been filtered before" + before + "after '" + after)
     return filteredData
   }
 
