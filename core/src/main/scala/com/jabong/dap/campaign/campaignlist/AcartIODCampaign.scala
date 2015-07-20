bd51c317792424f070335ff6d130052d429bd9f5
Rahul Aneja <rahul.aneja@jabong.com>
2015-07-20 17:35:49 +0530

    acart campaigns

--- core/src/main/scala/com/jabong/dap/campaign/campaignlist/AcartIODCampaign.scala
+++ core/src/main/scala/com/jabong/dap/campaign/campaignlist/AcartIODCampaign.scala
@@ -3,19 +3,19 @@ package com.jabong.dap.campaign.campaignlist
 import com.jabong.dap.campaign.data.CampaignOutput
 import com.jabong.dap.campaign.manager.CampaignProducer
 import com.jabong.dap.campaign.utils.CampaignUtils
-import com.jabong.dap.common.constants.campaign.{ SkuSelection, CustomerSelection, CampaignCommon }
+import com.jabong.dap.common.constants.campaign.{SkuSelection, CustomerSelection, CampaignCommon}
 import org.apache.spark.sql.DataFrame
 
 /**
  * Created by rahul for com.jabong.dap.campaign.campaignlist on 20/7/15.
  */
-class AcartFollowUpCampaign {
+class AcartIODCampaign {
 
   def runCampaign(salesCartData: DataFrame, orderItemData: DataFrame, itrData: DataFrame): Unit = {
 
     val invalidCustomerSelector = CampaignProducer.getFactory(CampaignCommon.CUSTOMER_SELECTOR)
       .getCustomerSelector(CustomerSelection.ACART)
-    //FIXME:Filter the order items data for last 3 days
+    //FIXME:Filter the order items data for 30 days
     val selectedCustomers = invalidCustomerSelector.customerSelection(salesCartData, orderItemData)
 
     //sku selection
@@ -27,4 +27,5 @@ class AcartFollowUpCampaign {
       + CampaignCommon.ACART_FOLLOWUP_CAMPAIGN + "/" + CampaignUtils.now(CampaignCommon.DATE_FORMAT))
 
   }
+
 }
