d973b19db08ab8c60bfd38548566591603315ede
Pooja Jain <pooja.jain@jabong.com>
2015-07-24 14:33:29 +0530

    Fixed MergeUtils Test Cases.

--- core/src/test/scala/com/jabong/dap/data/storage/merge/common/MergeUtilsTest.scala
+++ core/src/test/scala/com/jabong/dap/data/storage/merge/common/MergeUtilsTest.scala
@@ -1,7 +1,7 @@
 package com.jabong.dap.data.storage.merge.common
 
+import com.jabong.dap.common.SharedSparkContext
 import com.jabong.dap.common.json.JsonUtils
-import com.jabong.dap.common.{SharedSparkContext, Spark}
 import org.apache.spark.sql.DataFrame
 import org.scalatest.FlatSpec
 
@@ -12,7 +12,6 @@ class MergeUtilsTest extends FlatSpec with SharedSparkContext {
 
   override def beforeAll() {
     super.beforeAll()
-    val sqlContext = Spark.getSqlContext()
 
     df1 = JsonUtils.readFromJson("common/merge", "1")
     df2 = JsonUtils.readFromJson("common/merge", "2")
@@ -25,12 +24,6 @@ class MergeUtilsTest extends FlatSpec with SharedSparkContext {
     assert(mergedDF.collect.size == 4)
   }
 
-  "A merged DF" should "have size 3" in {
-    var mergedDF = MergeUtils.joinOldAndNewDF(df1, df1.schema, null, df1.schema, "name")
-    mergedDF.collect.foreach(println)
-    assert(mergedDF.collect.size == 3)
-  }
-
   "A Merged DF" should "have size 2" in {
     var mergedDF = MergeUtils.InsertUpdateMerge(null, df2, "name")
     mergedDF.collect.foreach(println)
@@ -39,6 +32,12 @@ class MergeUtilsTest extends FlatSpec with SharedSparkContext {
 
   "A Merged DF" should "have size 3" in {
     var mergedDF = MergeUtils.InsertUpdateMerge(df1, null, "name")
+    mergedDF.collect.foreach(println)
+    assert(mergedDF.collect.size == 3)
+  }
+
+  "A joined DF" should "have size 3" in {
+    var mergedDF = MergeUtils.joinOldAndNewDF(df1, df1.schema, null, df1.schema, "name")
     mergedDF.collect.foreach(println)
     assert(mergedDF.collect.size == 3)
   }
--- core/src/test/scala/com/jabong/dap/model/clickstream/UserDeviceMappingTest.scala
+++ core/src/test/scala/com/jabong/dap/model/clickstream/UserDeviceMappingTest.scala
@@ -1,21 +1,20 @@
 package com.jabong.dap.model.clickstream
 
+import com.jabong.dap.common.SharedSparkContext
 import com.jabong.dap.common.json.JsonUtils
-import com.jabong.dap.common.{SharedSparkContext, Spark}
 import com.jabong.dap.data.storage.DataSets
 import com.jabong.dap.model.clickstream.variables.UserDeviceMapping
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.sql.DataFrame
 import org.scalatest.FlatSpec
 
 class UserDeviceMappingTest extends FlatSpec with SharedSparkContext {
 
-  @transient var sqlContext: SQLContext = _
   @transient var dfInputUserDeviceMap: DataFrame = _
   @transient var dfOutputUserDeviceMap: DataFrame = _
 
   override def beforeAll() {
     super.beforeAll()
-    sqlContext = Spark.getSqlContext()
+
     dfInputUserDeviceMap = JsonUtils.readFromJson(DataSets.CLICKSTREAM, "userDeviceMappingInput")
     dfOutputUserDeviceMap = JsonUtils.readFromJson(DataSets.CLICKSTREAM, "userDeviceMappingOutput")
   }
