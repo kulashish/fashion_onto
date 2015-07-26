378ce47572160a3c5798486e3326fe732a0749e7
Rahul Aneja <rahul.aneja@jabong.com>
2015-07-26 21:07:26 +0530

    campaign manager changes

--- core/src/main/scala/com/jabong/dap/campaign/data/CampaignInput.scala
+++ core/src/main/scala/com/jabong/dap/campaign/data/CampaignInput.scala
@@ -97,4 +97,14 @@ object CampaignInput extends Logging {
     return null
   }
 
+  /**
+   * Load all campaign data
+   * @return dataframe with call campaigns data
+   */
+  def loadAllCampaignsData():DataFrame ={
+    val dateYesterday = TimeUtils.getDateAfterNDays(-1, "yyyy/MM/dd")
+    logger.info("Reading last day all campaigns data from hdfs")
+    val campaignData = DataReader.getDataFrame(DataSets.OUTPUT_PATH, "campaigns", "*", DataSets.DAILY_MODE, dateYesterday)
+    return campaignData
+  }
 }
