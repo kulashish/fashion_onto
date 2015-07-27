f9a4120ec13ab21cdafa4714a2068ac0bdb67438
Rahul Aneja <rahul.aneja@jabong.com>
2015-07-27 16:32:22 +0530

    added wishlist campaign file

---
+++ core/src/main/scala/com/jabong/dap/campaign/campaignlist/WishListCampaign.scala
@@ -0,0 +1,26 @@
+package com.jabong.dap.campaign.campaignlist
+
+import com.jabong.dap.campaign.manager.CampaignProducer
+import com.jabong.dap.common.constants.campaign.{CustomerSelection, CampaignCommon}
+import org.apache.spark.sql.DataFrame
+
+/**
+ * Created by rahul for com.jabong.dap.campaign.campaignlist on 27/7/15.
+ */
+class WishListCampaign {
+
+
+  def runCampaign(shortListFullData:DataFrame,itrSku30DaysData:DataFrame,itrSkuSimpleYesterdayData:DataFrame): Unit ={
+    val wishListCustomerSelector = CampaignProducer.getFactory(CampaignCommon.CUSTOMER_SELECTOR)
+      .getCustomerSelector(CustomerSelection.WISH_LIST)
+    val customerSelected =  wishListCustomerSelector.customerSelection(shortListFullData,30)
+
+    // call iod campaign
+    val wishListIODCampaign = new WishlistIODCampaign()
+    wishListIODCampaign.runCampaign(customerSelected,itrSku30DaysData,itrSkuSimpleYesterdayData)
+
+    // call lowstock campaign
+    val wishListLowStockCampaign = new WishlistLowStockCampaign()
+    wishListLowStockCampaign.runCampaign(customerSelected,itrSku30DaysData,itrSkuSimpleYesterdayData)
+  }
+}
