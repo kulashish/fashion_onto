942af9e3bb2397cfd2ac0a253e788df320ead4f2
raghu <raghu@raghu1381>
2015-07-28 14:55:14 +0530

    changes in surf1Campaign

--- core/src/main/scala/com/jabong/dap/campaign/campaignlist/Surf1Campaign.scala
+++ core/src/main/scala/com/jabong/dap/campaign/campaignlist/Surf1Campaign.scala
@@ -2,18 +2,21 @@ package com.jabong.dap.campaign.campaignlist
 
 import com.jabong.dap.campaign.manager.CampaignProducer
 import com.jabong.dap.common.constants.campaign.{ SkuSelection, CustomerSelection, CampaignCommon }
+import org.apache.spark.sql.DataFrame
 
 /**
  * Created by raghu on 24/7/15.
  */
 class Surf1Campaign {
 
-  val customerSelector = CampaignProducer.getFactory(CampaignCommon.CUSTOMER_SELECTOR).getCustomerSelector(CustomerSelection.YESTERDAY_SESSION)
+  def runCampaign(dfCustomerPageVisit: DataFrame, dfItrData: DataFrame, dfCustomer: DataFrame, dfSalesOrder: DataFrame, dfSalesOrderItem: DataFrame): Unit = {
 
-  val customerSurfData = customerSelector.customerSelection(null)
+    val customerSelector = CampaignProducer.getFactory(CampaignCommon.CUSTOMER_SELECTOR).getCustomerSelector(CustomerSelection.YESTERDAY_SESSION)
 
-  val dfSkuSelector = CampaignProducer.getFactory(CampaignCommon.SKU_SELECTOR).getSkuSelector(SkuSelection.SURF)
+    val customerSurfData = customerSelector.customerSelection(null)
 
-  val skuSelector = dfSkuSelector.skuFilter(customerSurfData, null, null)
+    val dfSkuSelector = CampaignProducer.getFactory(CampaignCommon.SKU_SELECTOR).getSkuSelector(SkuSelection.SURF)
 
+    val skuSelector = dfSkuSelector.skuFilter(dfCustomerPageVisit, dfItrData, dfCustomer, dfSalesOrder, dfSalesOrderItem)
+  }
 }
