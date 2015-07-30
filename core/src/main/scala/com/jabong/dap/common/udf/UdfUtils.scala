07963e74d968b2a19f98afd51fd6720d828de503
Pooja Jain <pooja.jain@jabong.com>
2015-07-30 13:19:03 +0530

    reformatting

--- core/src/main/scala/com/jabong/dap/common/udf/UdfUtils.scala
+++ core/src/main/scala/com/jabong/dap/common/udf/UdfUtils.scala
@@ -3,7 +3,7 @@ package com.jabong.dap.common.udf
 import java.sql.Timestamp
 import java.util.Date
 
-import com.jabong.dap.common.{StringUtils, ArrayUtils}
+import com.jabong.dap.common.{ StringUtils, ArrayUtils }
 import com.jabong.dap.common.time.{ TimeConstants, TimeUtils }
 import net.liftweb.json.JsonParser.ParseException
 import net.liftweb.json._
