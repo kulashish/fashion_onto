050bc872ab3326f1ada24279004fa425e6de9dfb
Rahul Aneja <rahul.aneja@jabong.com>
2015-07-30 13:16:05 +0530

    added past campaign check

--- core/src/main/scala/com/jabong/dap/campaign/campaignlist/InvalidLowStockCampaign.scala
+++ core/src/main/scala/com/jabong/dap/campaign/campaignlist/InvalidLowStockCampaign.scala
@@ -3,6 +3,7 @@ package com.jabong.dap.campaign.campaignlist
 import com.jabong.dap.campaign.data.CampaignOutput
 import com.jabong.dap.campaign.manager.CampaignProducer
 import com.jabong.dap.campaign.utils.CampaignUtils
+import com.jabong.dap.campaign.traceability.PastCampaignCheck
 import com.jabong.dap.common.constants.campaign.{ SkuSelection, CustomerSelection, CampaignCommon }
 import org.apache.spark.sql.DataFrame
 
@@ -16,7 +17,7 @@ class InvalidLowStockCampaign {
    * @param orderItemData
    * @param itrData
    */
-  def runCampaign(customerOrderData: DataFrame, orderItemData: DataFrame, itrData: DataFrame): Unit = {
+  def runCampaign(past30DayCampaignMergedData: DataFrame, customerOrderData: DataFrame, orderItemData: DataFrame, itrData: DataFrame): Unit = {
 
     val invalidCustomerSelector = CampaignProducer.getFactory(CampaignCommon.CUSTOMER_SELECTOR)
       .getCustomerSelector(CustomerSelection.INVALID)
@@ -24,6 +25,10 @@ class InvalidLowStockCampaign {
     //FIXME:Filter the order items data for last 30 days
     val selectedCustomers = invalidCustomerSelector.customerSelection(customerOrderData, orderItemData)
 
+    //past campaign check whether the campaign has been sent to customer in last 30 days
+    val pastCampaignCheck = new PastCampaignCheck()
+    pastCampaignCheck.campaignRefSkuCheck(past30DayCampaignMergedData, selectedCustomers,
+      CampaignCommon.campaignMailTypeMap.getOrElse(CampaignCommon.INVALID_LOWSTOCK_CAMPAIGN, 1000), 30)
     //sku selection
     val lowStock = CampaignProducer.getFactory(CampaignCommon.SKU_SELECTOR).getSkuSelector(SkuSelection.LOW_STOCK)
     val refSkus = lowStock.skuFilter(selectedCustomers, itrData)
