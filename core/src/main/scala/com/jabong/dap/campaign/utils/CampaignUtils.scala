d9cf3aa254070ee57fdc6cbc904b8630f215ae8c
Sachin Goyal <sachin.goyal@jabong.com>
2015-08-03 14:31:46 +0530

    fix for null fk_customer filtering for surf campaigns

--- core/src/main/scala/com/jabong/dap/campaign/utils/CampaignUtils.scala
+++ core/src/main/scala/com/jabong/dap/campaign/utils/CampaignUtils.scala
@@ -51,13 +51,28 @@ object CampaignUtils extends Logging {
         skuData(CustomerPageVisitVariables.BROWER_ID),
         skuData(CustomerPageVisitVariables.DOMAIN)
       )
-    val customerRefSku = customerFilteredData.orderBy($"${ProductVariables.SPECIAL_PRICE}".desc)
+    
+    // null or 0 FK_CUSTOMER
+    val deviceOnlyCustomerRefSku = customerFilteredData.filter(CustomerVariables.FK_CUSTOMER + " = 0  or " + CustomerVariables.FK_CUSTOMER + " is null")
+      .orderBy($"${ProductVariables.SPECIAL_PRICE}".desc)
+      .groupBy(CustomerPageVisitVariables.BROWER_ID).agg(first(ProductVariables.SKU)
+      as (CampaignMergedFields.REF_SKU1),
+        first(CustomerPageVisitVariables.BROWER_ID) as "device_id",
+        first(CustomerPageVisitVariables.DOMAIN) as CustomerPageVisitVariables.DOMAIN
+      )
+    
+    // non zero FK_CUSTOMER
+    
+    val registeredCustomerRefSku = customerFilteredData.filter(CustomerVariables.FK_CUSTOMER + " != 0  and " + CustomerVariables.FK_CUSTOMER + " is not null")
+      .orderBy($"${ProductVariables.SPECIAL_PRICE}".desc)
       .groupBy(CustomerVariables.FK_CUSTOMER).agg(first(ProductVariables.SKU)
         as (CampaignMergedFields.REF_SKU1),
         first(CustomerPageVisitVariables.BROWER_ID) as "device_id",
         first(CustomerPageVisitVariables.DOMAIN) as CustomerPageVisitVariables.DOMAIN
       )
 
+    val customerRefSku = deviceOnlyCustomerRefSku.unionAll(registeredCustomerRefSku)
+    
     return customerRefSku
 
   }
