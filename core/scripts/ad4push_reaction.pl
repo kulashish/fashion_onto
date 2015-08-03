9b5360bda0f47c7498e01df263b1415350c2f252
Sachin Goyal <sachin.goyal@jabong.com>
2015-08-03 12:34:15 +0530

    ad4push customer reaction pipeline fix

--- core/scripts/ad4push_reaction.pl
+++ core/scripts/ad4push_reaction.pl
@@ -17,11 +17,14 @@ system("lftp -c \"open -u jabong,oJei-va8opue7jey sftp://sftp.ad4push.msp.fr.cla
 system("lftp -c \"open -u jabong,oJei-va8opue7jey sftp://sftp.ad4push.msp.fr.clara.net ;  get  exports/exportMessagesReactions_517_$date_with_zero.csv; bye\"");
 
 # copy data to hdfs
-system("hadoop fs -mkdir /data/input/ad4push/reactions_android/daily/$date/");
+system("hadoop fs -mkdir -p /data/input/ad4push/reactions_android/daily/$date/");
 system("hadoop fs -copyFromLocal exportMessagesReactions_517_$date_with_zero.csv /data/input/ad4push/reactions_android/daily/$date/.");
 
-system("hadoop fs -mkdir /data/input/ad4push/reactions_ios/daily/$date/");
+system("hadoop fs -mkdir -p /data/input/ad4push/reactions_ios/daily/$date/");
 system("hadoop fs -copyFromLocal exportMessagesReactions_515_$date_with_zero.csv /data/input/ad4push/reactions_ios/daily/$date/.");
 
 # call ad4push pipeline
 system("perl /opt/alchemy-core/current/bin/run.pl -t prod -c Ad4pushCustReact");
+
+# copy processed data to ftp location
+system("perl /opt/alchemy-core/base/ftp_upload.pl -c ad4push_customer_response");
