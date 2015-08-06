bd538bb25c8569e480ed62668bc7fc5c1ed94f65
Pooja Jain <pooja.jain@jabong.com>
2015-08-06 14:30:53 +0530

    removed print Stmts

--- core/src/main/scala/com/jabong/dap/model/customer/data/CustomerDeviceMapping.scala
+++ core/src/main/scala/com/jabong/dap/model/customer/data/CustomerDeviceMapping.scala
@@ -1,12 +1,13 @@
 package com.jabong.dap.model.customer.data
 
-import com.jabong.dap.common.constants.variables.{ CustomerVariables, PageVisitVariables }
+import com.jabong.dap.common.constants.variables.{CustomerVariables, PageVisitVariables}
 import com.jabong.dap.common.time.{ TimeConstants, TimeUtils }
 import com.jabong.dap.common.udf.Udf
 import com.jabong.dap.common.{ OptionUtils, Spark }
 import com.jabong.dap.data.acq.common.VarInfo
 import com.jabong.dap.data.read.{ DataNotFound, DataReader, ValidFormatNotFound }
 import com.jabong.dap.data.storage.DataSets
+import com.jabong.dap.data.storage.merge.common.MergeUtils
 import com.jabong.dap.data.write.DataWriter
 import grizzled.slf4j.Logging
 import org.apache.spark.sql.DataFrame
@@ -133,7 +134,49 @@ object CustomerDeviceMapping extends Logging {
           col("BID") as PageVisitVariables.BROWSER_ID,
           when(col("APPTYPE").contains("ios"),lit("ios")).otherwise(col("APPTYPE")) as PageVisitVariables.DOMAIN
         )
-      df
+      // println("Total recs in DCF file initially: " + df.count())
+      // df.printSchema()
+      // df.show(9)
+
+      val dupFile = "/data/output/extras/duplicate/cust_email.csv"
+      val duplicate = Spark.getSqlContext().read.format("com.databricks.spark.csv").option("header", "true").option("delimiter", ",").load(dupFile)
+      // println("Total recs in duplicate file: " + duplicate.count())
+      // duplicate.printSchema()
+      // duplicate.show(9)
+
+      val NEW_RESPONSYS_ID = MergeUtils.NEW_ + CustomerVariables.RESPONSYS_ID
+      val NEW_ID_CUSTOMER = MergeUtils.NEW_ + CustomerVariables.ID_CUSTOMER
+      val NEW_EMAIL = MergeUtils.NEW_ + CustomerVariables.EMAIL
+      val NEW_BROWSER_ID = MergeUtils.NEW_ + PageVisitVariables.BROWSER_ID
+      val NEW_DOMAIN = MergeUtils.NEW_ + PageVisitVariables.DOMAIN
+
+      val correctRecs = df.join(duplicate, df(CustomerVariables.ID_CUSTOMER) === duplicate(CustomerVariables.ID_CUSTOMER) && df(CustomerVariables.EMAIL) === duplicate(CustomerVariables.EMAIL))
+      .select(
+          df(CustomerVariables.RESPONSYS_ID) as NEW_RESPONSYS_ID,
+          df(CustomerVariables.ID_CUSTOMER) as NEW_ID_CUSTOMER,
+          df(CustomerVariables.EMAIL) as NEW_EMAIL,
+          df(PageVisitVariables.BROWSER_ID) as NEW_BROWSER_ID,
+          df(PageVisitVariables.DOMAIN) as NEW_DOMAIN
+        )
+      // println("Total recs corrected: " + correctRecs.count())
+      // correctRecs.printSchema()
+      // correctRecs.show(9)
+
+      val res = df.join(correctRecs, df(CustomerVariables.ID_CUSTOMER) === correctRecs(NEW_ID_CUSTOMER), "leftouter")
+        .select(
+          coalesce(correctRecs(NEW_RESPONSYS_ID),df(CustomerVariables.RESPONSYS_ID)) as CustomerVariables.RESPONSYS_ID,
+          coalesce(correctRecs(NEW_ID_CUSTOMER),df(CustomerVariables.ID_CUSTOMER)) as CustomerVariables.ID_CUSTOMER,
+          coalesce(correctRecs(NEW_EMAIL),df(CustomerVariables.EMAIL)) as CustomerVariables.EMAIL,
+          coalesce(correctRecs(NEW_BROWSER_ID), df(PageVisitVariables.BROWSER_ID)) as PageVisitVariables.BROWSER_ID,
+          coalesce(correctRecs(NEW_DOMAIN), df(PageVisitVariables.DOMAIN)) as PageVisitVariables.DOMAIN
+        ).dropDuplicates()
+
+      // println("Total recs after correction: " + res.count())
+      // res.printSchema()
+      // res.show(9)
+
+      return res
+
     } catch {
       case e: DataNotFound =>
         logger.error("Data not found for the given path ")
