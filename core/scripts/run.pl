ad3636e62376bc01eed9567ca0572564cc118d10
Pooja Jain <pooja.jain@jabong.com>
2015-08-07 11:35:11 +0530

    changed the campaign quality check output

--- core/scripts/run.pl
+++ core/scripts/run.pl
@@ -139,7 +139,7 @@ if ($component eq "bob") {
      my $command = "$BASE_SPARK_SUBMIT $AMMUNITION $HIVE_JARS $CORE_JAR --component pushSurfCampaign --config $HDFS_CONF/config.json --pushCampaignsJson $HDFS_CONF/pushCampaigns.json";
      run_component($component, $command);
 }elsif ($component eq "mobilePushCampaignQuality") {
-     my $command = "$BASE_SPARK_SUBMIT $AMMUNITION $HIVE_JARS $CORE_JAR --component mobilePushCampaignQuality --config $HDFS_CONF/config.json --pushCampaignsJson $HDFS_CONF/pushCampaigns.json";
+     my $command = "$BASE_SPARK_SUBMIT $AMMUNITION $CORE_JAR --component mobilePushCampaignQuality --config $HDFS_CONF/config.json --pushCampaignsJson $HDFS_CONF/pushCampaigns.json";
      run_component($component, $command);
  } else {
     print "not a valid component\n";
