7805011d374332c3d5a9f58dd33e237568b99393
Rahul Aneja <rahul.aneja@jabong.com>
2015-08-18 11:03:58 +0530

    added acart campaign quality check

--- core/src/main/scala/com/jabong/dap/quality/campaign/ACartPushCampaignQuality.scala
+++ core/src/main/scala/com/jabong/dap/quality/campaign/ACartPushCampaignQuality.scala
@@ -2,45 +2,40 @@ package com.jabong.dap.quality.campaign
 
 import com.jabong.dap.campaign.data.CampaignInput
 import com.jabong.dap.common.constants.SQL
-import com.jabong.dap.common.constants.campaign.CampaignCommon
+import com.jabong.dap.common.constants.campaign.{CampaignMergedFields, CampaignCommon}
 import com.jabong.dap.common.constants.variables.SalesOrderVariables
 import com.jabong.dap.common.constants.variables.ACartVariables
 import com.jabong.dap.common.time.TimeUtils
+import com.jabong.dap.common.udf.Udf
+import grizzled.slf4j.Logging
 import org.apache.spark.sql.DataFrame
 
 /**
  * Created by rahul on 14/8/15.
  */
-object ACartPushCampaignQuality {
-  /** gives random selected rows from DataFrame
-    * @param df
-    * @param fraction 1 means give all data, .5 means give half of the data selecting in random
-    * @return
-    */
-  def getSample(df:DataFrame, fraction:Double):DataFrame={
-    df.sample(false,fraction)
-  }
+object ACartPushCampaignQuality extends BaseCampaignQuality with Logging {
 
   /** Consists of all the validation components for Backward test
     * @param salesCartDF
-    * @param yesterdayItr
     * @param sampleAcartFollowUp
     * @return
     */
-  def validateAcartFollowup(salesCartDF:DataFrame,yesterdayItr:DataFrame,sampleAcartFollowUp:DataFrame): Boolean = {
+  def validateAcartFollowup(salesCartDF:DataFrame,sampleAcartFollowUp:DataFrame): Boolean = {
     if((salesCartDF==null) || sampleAcartFollowUp == null)
       return sampleAcartFollowUp == null
-    return validateAbandonedCart(salesCartDF,sampleAcartFollowUp) && SkuSelectionQuality.validateFollowupStock(yesterdayItr,sampleAcartFollowUp)
+    return validateAbandonedCart(salesCartDF,sampleAcartFollowUp)
+    //&& SkuSelectionQuality.validateFollowupStock(yesterdayItr,sampleAcartFollowUp)
   }
 
-  def validateAcartLowStock(salesCartDF:DataFrame,yesterdayItr:DataFrame,sampleAcartLowStock:DataFrame): Boolean = {
+  def validateAcartLowStock(salesCartDF:DataFrame,sampleAcartLowStock:DataFrame): Boolean = {
     if((salesCartDF==null) || sampleAcartLowStock == null)
       return sampleAcartLowStock == null
-    return validateAbandonedCart(salesCartDF,sampleAcartLowStock) && SkuSelectionQuality.validateLowStock(yesterdayItr,sampleAcartLowStock)
+    return validateAbandonedCart(salesCartDF,sampleAcartLowStock)
+    //&& SkuSelectionQuality.validateLowStock(yesterdayItr,sampleAcartLowStock)
   }
 
 
-  def validateAcartDaily(salesCartDF:DataFrame,yesterdayItr:DataFrame,sampleAcartDaily:DataFrame): Boolean = {
+  def validateAcartDaily(salesCartDF:DataFrame,sampleAcartDaily:DataFrame): Boolean = {
     if((salesCartDF==null) || sampleAcartDaily == null)
       return sampleAcartDaily == null
     return validateAbandonedCart(salesCartDF,sampleAcartDaily)
@@ -52,9 +47,14 @@ object ACartPushCampaignQuality {
     * @return
     */
   def validateAbandonedCart(salesCartDF:DataFrame,sampleCampaignOutput:DataFrame): Boolean = {
-    val notAbandaonedCartCustomers = salesCartDF.join(sampleCampaignOutput,salesCartDF(SalesOrderVariables.FK_CUSTOMER)===sampleCampaignOutput(SalesOrderVariables.FK_CUSTOMER),SQL.INNER)
-      .filter(ACartVariables.ACART_STATUS+"!='active'")
-    return (notAbandaonedCartCustomers.count == 0)
+    val updatedSalesCart = salesCartDF.select(salesCartDF(ACartVariables.FK_CUSTOMER),
+      Udf.skuFromSimpleSku(salesCartDF(ACartVariables.SKU_SIMPLE)) as ACartVariables.SKU_SIMPLE)
+
+    val abandaonedCartCustomers = updatedSalesCart.join(sampleCampaignOutput,updatedSalesCart(SalesOrderVariables.FK_CUSTOMER)===sampleCampaignOutput(CampaignMergedFields.CUSTOMER_ID)
+      && sampleCampaignOutput(CampaignMergedFields.REF_SKU1)=== (updatedSalesCart(ACartVariables.SKU_SIMPLE)) ,SQL.INNER)
+      .filter(ACartVariables.ACART_STATUS+"='active'").dropDuplicates()
+
+    return (abandaonedCartCustomers.count == sampleCampaignOutput.count)
   }
 
   /**
@@ -62,14 +62,14 @@ object ACartPushCampaignQuality {
    * @param date in 2015/08/01 format
    * @return
    */
-  def getInputOutput(date:String):(DataFrame,DataFrame,DataFrame)={
+  def getInputOutput(date:String):(DataFrame,DataFrame,DataFrame,DataFrame)={
     val salesCart30Days = CampaignInput.loadLast30daysAcartData()
-    val yesterDayItr30Days = CampaignInput.load30DayItrSkuSimpleData()
-    val yesterDayItrData = CampaignInput.loadYesterdayItrSimpleData()
+   // val yesterDayItr30Days = CampaignInput.load30DayItrSkuSimpleData()
+   // val yesterDayItrData = CampaignInput.loadYesterdayItrSimpleData()
     val acartFollowUp = CampaignInput.getCampaignData(CampaignCommon.ACART_DAILY_CAMPAIGN,date,CampaignCommon.VERY_LOW_PRIORITY)
     val acartDaily = CampaignInput.getCampaignData(CampaignCommon.ACART_DAILY_CAMPAIGN,date,CampaignCommon.VERY_LOW_PRIORITY)
     val acartLowStock = CampaignInput.getCampaignData(CampaignCommon.ACART_DAILY_CAMPAIGN,date,CampaignCommon.VERY_LOW_PRIORITY)
-    return(salesCart30Days,yesterDayItrData,acartFollowUp)
+    return(salesCart30Days,acartFollowUp,acartDaily,acartLowStock)
   }
 
   /**Entry point
@@ -80,10 +80,15 @@ object ACartPushCampaignQuality {
     * @return
     */
   def backwardTest(date:String, fraction:Double):Boolean = {
-    val (salesCartDF,orderDF,cancelRetargetDF) = getInputOutput(date)
-    val sampleAcartFollowUpDF = getSample(cancelRetargetDF,fraction)
-    validateAcartFollowup(salesCartDF,orderDF,sampleAcartFollowUpDF)
-    validateAcartLowStock(salesCartDF,orderDF,sampleAcartFollowUpDF)
-    validateAcartDaily(salesCartDF,orderDF,sampleAcartFollowUpDF)
+    val (salesCartDF,acartFollowUp,acartDaily,acartLowStock) = getInputOutput(date)
+    val sampleAcart = getSample(acartFollowUp,fraction)
+    val sampleAcartDaily = getSample(acartDaily,fraction)
+    val sampleAcartLowStock = getSample(acartLowStock,fraction)
+
+    val acartFollowUpStatus  = validateAcartFollowup(salesCartDF,sampleAcart)
+    val acartDailyStatus = validateAcartLowStock(salesCartDF,sampleAcartDaily)
+    val acartLowStockStatus = validateAcartDaily(salesCartDF,sampleAcartLowStock)
+    logger.info("acartFollowUpStatus:-"+ acartFollowUpStatus +"acartDailyStatus:-"+acartDailyStatus+"acartLowStockStatus:-"+acartLowStockStatus)
+    return acartFollowUpStatus && acartDailyStatus && acartLowStockStatus
   }
 }
