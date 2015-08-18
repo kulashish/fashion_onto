e3715c2ba26c33f9f86ed1f69a31e0df01c27f7f
mubarakm <md.mubarak@jabong.com>
2015-08-18 11:00:46 +0530

    removed sku select method

--- core/src/main/scala/com/jabong/dap/quality/campaign/LiveInvalid.scala
+++ core/src/main/scala/com/jabong/dap/quality/campaign/LiveInvalid.scala
@@ -16,75 +16,27 @@ import org.apache.spark.sql.functions._
  */
 object LiveInvalid {
 
+
+  /**
+   *
+   * @param sales
+   * @param campaign
+   * @return
+   */
   def checkInvalidOrder(sales: DataFrame, campaign: DataFrame): Boolean ={
     val joined = sales.join(campaign, sales(SalesOrderVariables.FK_CUSTOMER) === campaign(CampaignMergedFields.CUSTOMER_ID) &&
       sales(SalesOrderItemVariables.SKU) === campaign(CampaignMergedFields.REF_SKU1) )
                     .select(CampaignMergedFields.CUSTOMER_ID,
                             SalesOrderItemVariables.FK_SALES_ORDER_ITEM_STATUS,
-                            SalesOrderItemVariables.UPDATED_AT).orderBy(desc(SalesOrderItemVariables.UPDATED_AT))
-
+                            SalesOrderItemVariables.CREATED_AT).orderBy(desc(SalesOrderItemVariables.CREATED_AT))
+    //TODO check whether to use updated_at or created_at
     val grouped = joined.groupBy(CampaignMergedFields.CUSTOMER_ID).agg(first(SalesOrderItemVariables.FK_SALES_ORDER_ITEM_STATUS) as "status")
 
     val fil = grouped.filter(grouped("status").!==(OrderStatus.CANCEL_PAYMENT_ERROR) || grouped("status").!==(OrderStatus.INVALID))
-    /* val map = joined.map(e => (e(0).toString ->(e(1).toString,e(2).toString) ))
-    val x = map.groupByKey()
-    val y = x.map(e=> (e._1, checkOrders(e._2)))*/
     return fil.count() == 0
-  }
-
-/*  def checkOrders(orders: Iterable[(String, String)]): Boolean ={
-
-      orders.toList.foreach{
-          e => val (k, date) = e
-
-            if(Integer.parseInt(k) != OrderStatus.CANCEL_PAYMENT_ERROR || Integer.parseInt(k) != OrderStatus.CANCEL_PAYMENT_ERROR){
-            return true
-          } else{
-            return false
-          }
-      }
-
-    return false
-  }*/
-
-  def checkLowStock(itr: DataFrame, campaign: DataFrame): Boolean={
-    val joined = itr.join(campaign, itr(ITR.CONFIG_SKU) === campaign(CampaignMergedFields.REF_SKU1))
-      .select(CampaignMergedFields.REF_SKU1,
-        ITR.QUANTITY)
-    joined.rdd.foreach(e => if(Integer.parseInt(e(1).toString)> CampaignCommon.LOW_STOCK_VALUE){
-      return false
-    })
-    return true
+    //TODO call the lowstock and followup methods from SKUSelectionQuality
   }
 
 
-  def checkFollowupStock(itr: DataFrame, campaign: DataFrame): Boolean={
-    val joined = itr.join(campaign, itr(ITR.CONFIG_SKU) === campaign(CampaignMergedFields.REF_SKU1))
-      .select(CampaignMergedFields.REF_SKU1,
-        ITR.QUANTITY)
-    joined.rdd.foreach(e => if(Integer.parseInt(e(1).toString)<= CampaignCommon.FOLLOW_UP_STOCK_VALUE){
-      return false
-    })
-    return true
-  }
-
-
-  def main (args: Array[String]) {
-    val conf = new SparkConf().setAppName("SparkExamples")
-    Spark.init(conf)
-    val invLow = Spark.getSqlContext().read.parquet("").sample(true,.10)
-    val invFol = Spark.getSqlContext().read.parquet("").sample(true,.10)
-    val itr = Spark.getSqlContext().read.format(DataSets.ORC).load("")
-    val salesOrder = Spark.getSqlContext().read.parquet("")
-    val salesOrderItem = Spark.getSqlContext().read.parquet("")
-    val sales = salesOrder.join(salesOrderItem, salesOrder(SalesOrderVariables.ID_SALES_ORDER) === salesOrderItem(SalesOrderItemVariables.FK_SALES_ORDER))
-                  .select(SalesOrderVariables.FK_CUSTOMER,
-                          SalesOrderItemVariables.SKU,
-                          SalesOrderItemVariables.FK_SALES_ORDER_ITEM_STATUS)
-    val res = checkInvalidOrder(sales, invFol)
-
-
-
-  }
 
 }
