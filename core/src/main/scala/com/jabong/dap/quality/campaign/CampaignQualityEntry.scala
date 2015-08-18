46bf28897c92726049b3d4f60373ca4dd0ce455e
Rahul Aneja <rahul.aneja@jabong.com>
2015-08-18 11:04:24 +0530

    added campaign quality entry changes

--- core/src/main/scala/com/jabong/dap/quality/campaign/CampaignQualityEntry.scala
+++ core/src/main/scala/com/jabong/dap/quality/campaign/CampaignQualityEntry.scala
@@ -15,8 +15,25 @@ object CampaignQualityEntry extends Logging{
 
       logger.info("Campaign Quality Triggered")
       val incrDate = OptionUtils.getOptValue(paramInfo.incrDate, TimeUtils.YESTERDAY_FOLDER)
-
-      ReturnReTargetQuality.backwardTest(incrDate,OptionUtils.getOptValue(paramInfo.fraction,DEFAULT_FRACTION).toDouble)
-      CancelReTargetQuality.backwardTest(incrDate,OptionUtils.getOptValue(paramInfo.fraction,DEFAULT_FRACTION).toDouble)
+      val fraction = OptionUtils.getOptValue(paramInfo.fraction,DEFAULT_FRACTION).toDouble
+      var status: Boolean = true
+      if(!ReturnReTargetQuality.backwardTest(incrDate, fraction)){
+        logger.info("ReturnReTargetQuality failed for:-"+ incrDate)
+        status = false
+      }
+      if(CancelReTargetQuality.backwardTest(incrDate, fraction)){
+        logger.info("CancelReTargetQuality failed for:-"+ incrDate)
+        status = false
+      }
+      if(ACartPushCampaignQuality.backwardTest(incrDate, fraction)){
+        logger.info("ACartPushCampaignQuality failed for:-"+ incrDate)
+        status = false
+      }
+      if(status == false) {
+        throw new FailedStatusException
+      }
     }
+
 }
+
+class FailedStatusException extends Exception
