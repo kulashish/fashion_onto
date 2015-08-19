011df140e81fc138abfd31c97702081e69f726de
Rahul Aneja <rahul.aneja@jabong.com>
2015-08-19 12:29:23 +0530

    fixed the code

--- core/src/main/scala/com/jabong/dap/quality/campaign/InvalidLowStockQuality.scala
+++ core/src/main/scala/com/jabong/dap/quality/campaign/InvalidLowStockQuality.scala
@@ -34,7 +34,7 @@ object InvalidLowStockQuality extends BaseCampaignQuality {
   def getInputOutput(date: String = TimeUtils.YESTERDAY_FOLDER): (DataFrame, DataFrame, DataFrame) = {
     val orderItemDF = CampaignQualityEntry.orderItem30DaysData
 
-    val fullOrderData = CampaignQualityEntry.last30DaysOrderData
+    val fullOrderData = CampaignQualityEntry.last30DaysOrderData.select(SalesOrderVariables.ID_SALES_ORDER,SalesOrderVariables.FK_CUSTOMER)
 
     val orderItemJoined = orderItemDF.join(fullOrderData, fullOrderData(SalesOrderVariables.ID_SALES_ORDER) === orderItemDF(SalesOrderItemVariables.FK_SALES_ORDER))
 
