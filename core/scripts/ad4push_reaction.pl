6691b838e2a151aabb09d4665c5191cbc459290f
Pooja Jain <pooja.jain@jabong.com>
2015-08-20 16:19:59 +0530

    fixed path for ftp_upload

--- core/scripts/ad4push_reaction.pl
+++ core/scripts/ad4push_reaction.pl
@@ -27,4 +27,4 @@ system("hadoop fs -copyFromLocal exportMessagesReactions_515_$date_with_zero.csv
 system("perl /opt/alchemy-core/current/bin/run.pl -t prod -c Ad4pushCustReact");
 
 # copy processed data to ftp location
-system("perl /opt/alchemy-core/base/ftp_upload.pl -c ad4push_customer_response");
+system("perl /opt/alchemy-core/current/bin/ftp_upload.pl -c ad4push_customer_response");
