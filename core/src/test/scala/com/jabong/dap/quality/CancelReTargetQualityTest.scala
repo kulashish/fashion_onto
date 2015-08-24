d46c7bfd9aa64dd0d54f5c853e8dcffad16c9627
Rahul Aneja <rahul.aneja@jabong.com>
2015-08-24 14:30:06 +0530

    reformat the code

--- core/src/test/scala/com/jabong/dap/quality/CancelReTargetQualityTest.scala
+++ core/src/test/scala/com/jabong/dap/quality/CancelReTargetQualityTest.scala
@@ -10,15 +10,15 @@ import org.scalatest.FlatSpec
 /**
  * Created by Kapil.Rajak on 12/8/15.
  */
-class CancelReTargetQualityTest  extends FlatSpec with SharedSparkContext {
+class CancelReTargetQualityTest extends FlatSpec with SharedSparkContext {
   "getSample" should "behave in expected way" in {
-    val df= JsonUtils.readFromJson(AD4PUSH, "reduceIn", dfFromCsv)
-    val sample = CancelReTargetQuality.getSample(df,.5)
-    assert(Math.abs((df.count()/2) - sample.count())<=1)
+    val df = JsonUtils.readFromJson(AD4PUSH, "reduceIn", dfFromCsv)
+    val sample = CancelReTargetQuality.getSample(df, .5)
+    assert(Math.abs((df.count() / 2) - sample.count()) <= 1)
 
-    val sample1By4 = CancelReTargetQuality.getSample(df,.25)
-    assert(Math.abs((df.count()/4) - sample1By4.count())<=1)
+    val sample1By4 = CancelReTargetQuality.getSample(df, .25)
+    assert(Math.abs((df.count() / 4) - sample1By4.count()) <= 1)
 
-    assert(CancelReTargetQuality.getSample(df,1).collect().toSet.equals(df.collect().toSet))
+    assert(CancelReTargetQuality.getSample(df, 1).collect().toSet.equals(df.collect().toSet))
   }
 }
