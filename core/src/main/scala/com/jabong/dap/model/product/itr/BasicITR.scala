03f5e0e65fb2b2824b5d56010d000ec1bdabaaab
Rahul Aneja <rahul.aneja@jabong.com>
2015-08-25 11:08:58 +0530

    added mvp and price band in base itr

--- core/src/main/scala/com/jabong/dap/model/product/itr/BasicITR.scala
+++ core/src/main/scala/com/jabong/dap/model/product/itr/BasicITR.scala
@@ -1,5 +1,7 @@
 package com.jabong.dap.model.product.itr
 
+import java.math
+
 import com.jabong.dap.common.constants.SQL
 import com.jabong.dap.common.constants.config.ConfigConstants
 import com.jabong.dap.common.OptionUtils
@@ -61,9 +63,85 @@ object BasicITR extends Logging {
         ITR.QUANTITY -> 0
       ))
 
-    itr.write.mode(saveMode).format(DataSets.ORC).save(getPath(false, incrDate))
+    val mvpUDF = udf(mvp)
+    val priceBandUDF = udf(priceBandFunc)
 
-    itr.
+    val priceBandDF = PriceBand.preparePriceBrand()
+    val priceBandMVPDF = itr.join(
+      priceBandDF,
+      itr.col(ITR.REPORTING_CATEGORY) === priceBandDF.col("bandCategory"),
+      SQL.LEFT_OUTER
+    ).
+      where(itr.col(ITR.BRICK) === priceBandDF.col("bandBrick")).
+      withColumn(ITR.MVP, mvpUDF(
+      col("priceBandA"),
+      col("priceBandB"),
+      col("priceBandC"),
+      col("priceBandD"),
+      col("priceBandE"),
+      col(ITR.SPECIAL_PRICE)
+    )).
+      withColumn(ITR.PRICE_BAND, priceBandUDF(
+      col("priceBandA"),
+      col("priceBandB"),
+      col("priceBandC"),
+      col("priceBandD"),
+      col("priceBandE"),
+      col(ITR.SPECIAL_PRICE)
+    ))
+
+    val itrDF = priceBandMVPDF.select(
+      ITR.JABONG_CODE,
+      ITR.VENDOR_ITEM_NO,
+      ITR.HEEL_HEIGHT,
+      ITR.SLEEVE,
+      ITR.FIT,
+      ITR.NECK,
+      ITR.STYLE,
+      ITR.MATERIAL,
+      ITR.SUPPLIER_COLOR,
+      ITR.STYLE_NAME,
+      ITR.SUPPLIER_STYLE_CODE,
+      ITR.DISPATCH_LOCATION,
+      ITR.PACKAGING,
+      ITR.REPORTING_SUBCATEGORY,
+      ITR.REPORTING_CATEGORY,
+      ITR.BRAND_TYPE,
+      ITR.ITEM_TYPE,
+      ITR.SEASON,
+      ITR.MRP_PRICE,
+      ITR.COLOR,
+      ITR.SIZE,
+      ITR.PET_STYLE_CODE,
+      ITR.GENDER,
+      ITR.BRICK,
+      ITR.CLASS,
+      ITR.FAMILY,
+      ITR.SEGMENT,
+      ITR.BUSINESS_UNIT,
+      ITR.ID_CATALOG_SIMPLE,
+      ITR.SPECIAL_TO_DATE,
+      ITR.SPECIAL_FROM_DATE,
+      ITR.SPECIAL_PRICE,
+      ITR.SIMPLE_SKU,
+      ITR.QUANTITY,
+      ITR.ID_CATALOG_CONFIG,
+      ITR.PRODUCT_NAME,
+      ITR.ACTIVATED_AT,
+      ITR.CONFIG_SKU,
+      ITR.PRODUCT_URL,
+      ITR.SPECIAL_MARGIN,
+      ITR.MARGIN,
+      ITR.MVP,
+      ITR.PRICE_ON_SITE,
+      ITR.PRICE_BAND,
+      ITR.SUPPLIER_STATUS,
+      ITR.BRAND_NAME
+    ).cache()
+
+    itrDF.write.mode(saveMode).format(DataSets.ORC).save(getPath(false, incrDate))
+
+    itrDF.
       groupBy(ITR.CONFIG_SKU).
       agg(
         first(ITR.BRAND_NAME) as ITR.BRAND_NAME,
@@ -92,5 +170,70 @@ object BasicITR extends Logging {
       return PathBuilder.buildPath(ConfigConstants.OUTPUT_PATH, "itr", "basic", DataSets.DAILY_MODE, TimeUtils.getDateAfterNDays(-1, TimeConstants.DATE_FORMAT))
     }
 
+  }
+
+  /**
+   * Convert string to tuple2
+   * @param priceBand
+   * @return Tuple2
+   */
+  def getRange(priceBand: String): (math.BigDecimal, math.BigDecimal) = {
+    val range = priceBand.split("-")
+    new Tuple2(new math.BigDecimal(range(0)), new math.BigDecimal(range(1)))
+  }
+
+  /**
+   * Calculate mvp
+   * @return String
+   */
+  val mvp = (priceBandA: String,
+             priceBandB: String,
+             priceBandC: String,
+             priceBandD: String,
+             priceBandE: String,
+             specialPrice: java.math.BigDecimal) => {
+    val aRange = getRange(priceBandA)
+    val bRange = getRange(priceBandB)
+    val cRange = getRange(priceBandC)
+    val dRange = getRange(priceBandD)
+
+    if ((specialPrice == 0.0) || (specialPrice.compareTo(aRange._1) >= 0 && specialPrice.compareTo(aRange._2) <= 0) ||
+      (specialPrice.compareTo(bRange._1) >= 0 && specialPrice.compareTo(bRange._2) <= 0)) {
+      "mass"
+    } else if ((specialPrice.compareTo(cRange._1) >= 0 && specialPrice.compareTo(cRange._2) <= 0) ||
+      (specialPrice.compareTo(dRange._1) >= 0 && specialPrice.compareTo(dRange._2) <= 0)) {
+      "value"
+    } else {
+      "premium"
+    }
+  }
+
+  /**
+   * Calculate price band
+   * @return String
+   */
+  val priceBandFunc = (priceBandA: String,
+                       priceBandB: String,
+                       priceBandC: String,
+                       priceBandD: String,
+                       priceBandE: String,
+                       specialPrice: math.BigDecimal) => {
+
+    val aRange = getRange(priceBandA)
+    val bRange = getRange(priceBandB)
+    val cRange = getRange(priceBandC)
+    val dRange = getRange(priceBandD)
+
+    if (specialPrice == 0.0 || (specialPrice.compareTo(aRange._1) >= 0 && specialPrice.compareTo(aRange._2) <= 0)) {
+      "A"
+    } else if (specialPrice.compareTo(bRange._1) >= 0 && specialPrice.compareTo(bRange._2) <= 0) {
+      "B"
+    } else if (specialPrice.compareTo(cRange._1) >= 0 && specialPrice.compareTo(cRange._2) <= 0) {
+      "C"
+    } else if (specialPrice.compareTo(dRange._1) >= 0 && specialPrice.compareTo(dRange._2) <= 0) {
+      "D"
+    } else {
+      "E"
+    }
   }
 }
