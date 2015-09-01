acbe6526a4a00280f9d877bcf2ed3c5ff8389daa
Rahul Aneja <rahul.aneja@jabong.com>
2015-09-01 17:55:48 +0530

    change group by to reduceby

--- core/src/main/scala/com/jabong/dap/campaign/recommendation/generator/CommonRecommendation.scala
+++ core/src/main/scala/com/jabong/dap/campaign/recommendation/generator/CommonRecommendation.scala
@@ -181,8 +181,8 @@ class CommonRecommendation extends Logging {
 
     // import sqlContext.implicits._
     //  val recommendationOutput = mappedRecommendationInput.reduceByKey((x,y)=>generateSku(x,y))
-    val recommendationOutput = mappedRecommendationInput.groupByKey().map{ case (key, value) => (key, genSku(value).toList) }
-    // val recommendationOutput = mappedRecommendationInput.map{case (key,value) => (key,Array(value))}.reduceByKey(_++_).map{ case (key, value) => (key, genSku(value).toList) }
+    //val recommendationOutput = mappedRecommendationInput.groupByKey().map{ case (key, value) => (key, genSku(value).toList) }
+    val recommendationOutput = mappedRecommendationInput.map{case (key,value) => (key,Array(value))}.reduceByKey(_++_).map{ case (key, value) => (key, genSku(value).toList) }
     println(recommendationOutput.toDebugString)
     //recommendationOutput.flatMapValues(identity).collect().foreach(println)
     // val recommendations = recommendationOutput.flatMap{case(key,value)=>(value.map( value => (key._1.toString,key._2.asInstanceOf[Long],value._1,value._2.sortBy(-_._1))))}
