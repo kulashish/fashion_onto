1c65c7ca088d748f4b855aad008859c5c1079896
Rahul Aneja <rahul.aneja@jabong.com>
2015-09-01 11:58:00 +0530

    added recommendation generate test cases

---
+++ core/src/test/scala/com/jabong/dap/campaign/recommendation/generate/CommonRecommendationTest.scala
@@ -0,0 +1,77 @@
+package com.jabong.dap.campaign.recommendation.generate
+
+import com.jabong.dap.campaign.recommendation.BasicRecommender
+import com.jabong.dap.campaign.recommendation.generator.CommonRecommendation
+import com.jabong.dap.common.constants.campaign.Recommendation
+import com.jabong.dap.common.json.JsonUtils
+import com.jabong.dap.common.time.{TimeConstants, TimeUtils}
+import com.jabong.dap.common.{NullInputException, Spark, SharedSparkContext}
+import com.jabong.dap.data.storage.DataSets
+import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by rahul aneja  on 28/8/15.
+ */
+class CommonRecommendationTest  extends FlatSpec with SharedSparkContext with Matchers {
+
+  @transient var sqlContext: SQLContext = _
+  @transient var itrDataFrame: DataFrame = _
+  @transient var orderItemDataFrame: DataFrame = _
+  @transient var inventoryCheckInput: DataFrame = _
+  var commonRecommendation: CommonRecommendation = _
+  var days: Int = _
+
+  override def beforeAll() {
+    super.beforeAll()
+    sqlContext = Spark.getSqlContext()
+    days = TimeUtils.daysFromToday(TimeUtils.getDate("2015-08-27", TimeConstants.DATE_FORMAT))
+    commonRecommendation = new CommonRecommendation()
+    orderItemDataFrame = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "sales_order_item_weekly_average_sales")
+    inventoryCheckInput = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "inventory_check_input")
+    itrDataFrame = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "basic_sku_itr")
+  }
+
+  "no order data frame Data Frame" should "return null data frame" in {
+    val expectedValue = commonRecommendation.createWeeklyAverageSales(null)
+    assert(expectedValue == null)
+  }
+
+  "last seven days order item dataframe" should "dataframe with sku SO596WA65JLIINDFAS and weekly average sale" in {
+    val expectedValue = commonRecommendation.createWeeklyAverageSales(orderItemDataFrame)
+    val expectedSku = expectedValue.filter(Recommendation.SALES_ORDER_ITEM_SKU + " = 'SO596WA65JLIINDFAS'")
+    assert(expectedValue.count() == 1)
+  }
+
+  "last seven days order item dataframe" should "dataframe without sku ES418WA79UAUINDFAS weekly average sale" in {
+    val expectedValue = commonRecommendation.createWeeklyAverageSales(orderItemDataFrame)
+    val expectedSku = expectedValue.filter(Recommendation.SALES_ORDER_ITEM_SKU + " = 'ES418WA79UAUINDFAS'")
+    assert(expectedSku.count() == 0)
+  }
+
+  "last seven days with weekly average sales and 30 days order item dataframe" should "adding weekly average 30 days " in {
+    val expectedValue = commonRecommendation.addWeeklyAverageSales(commonRecommendation.createWeeklyAverageSales(orderItemDataFrame),
+      commonRecommendation.topProductsSold(orderItemDataFrame))
+    val expectedSku = expectedValue.filter(Recommendation.SALES_ORDER_ITEM_SKU + " = 'ES418WA79UAUINDFAS'")
+    assert(expectedSku.count() == 1)
+  }
+
+  "null weekly average sales and 30 days order item dataframe" should " return NullInputException " in {
+    a[NullInputException] should be thrownBy {
+      commonRecommendation.addWeeklyAverageSales(null,
+        commonRecommendation.topProductsSold(orderItemDataFrame))
+    }
+  }
+
+  "null input dataframe in inventory check " should " return NullInputException " in {
+    a[NullInputException] should be thrownBy {
+      commonRecommendation.inventoryCheck(null)
+    }
+  }
+
+  "input dataframe in inventory check " should " return skus with desired inventory" in {
+    val expectedValue = commonRecommendation.inventoryCheck(inventoryCheckInput)
+    assert(expectedValue == null)
+
+  }
+}
---
+++ core/src/test/scala/com/jabong/dap/campaign/recommendation/generate/RecommendationUtilsTest.scala
@@ -0,0 +1,34 @@
+package com.jabong.dap.campaign.recommendation.generate
+
+import com.jabong.dap.campaign.recommendation.generator.RecommendationUtils
+import org.scalatest.FlatSpec
+
+/**
+ * Created by rahul aneja on 31/8/15.
+ */
+class RecommendationUtilsTest extends FlatSpec {
+
+
+  "No category  but stock is less than weekly average " should "return false means should  be removed" in {
+    val inventorySoldStatus = RecommendationUtils.inventoryFilter(null,5,20,40)
+    assert(inventorySoldStatus == false)
+  }
+
+  "No category  but stock is equal than weekly average " should "return true means should not be filtered" in {
+    val inventorySoldStatus = RecommendationUtils.inventoryFilter(null,5,40,40)
+    assert(inventorySoldStatus == true)
+  }
+
+
+  "Sunglasses category  and weekly average is null " should "return true means should not be filtered" in {
+    val inventorySoldStatus = RecommendationUtils.inventoryFilter("SUNGLASSES",5,40 , null)
+    assert(inventorySoldStatus == true)
+  }
+
+
+  "Toys category  and weekly average is null " should "return false means should not be filtered" in {
+    val inventorySoldStatus = RecommendationUtils.inventoryFilter("TOYS",8,10 ,null)
+    assert(inventorySoldStatus == false)
+  }
+
+}
