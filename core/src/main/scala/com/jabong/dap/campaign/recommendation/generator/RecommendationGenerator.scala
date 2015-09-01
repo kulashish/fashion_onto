13e4e9da87e5fff0def138777a485c81a67f541d
Rahul Aneja <rahul.aneja@jabong.com>
2015-09-01 15:52:53 +0530

    added throws exception annotations

--- core/src/main/scala/com/jabong/dap/campaign/recommendation/generator/RecommendationGenerator.scala
+++ core/src/main/scala/com/jabong/dap/campaign/recommendation/generator/RecommendationGenerator.scala
@@ -1,6 +1,6 @@
 package com.jabong.dap.campaign.recommendation.generator
 
-import com.jabong.dap.common.OptionUtils
+import com.jabong.dap.common.{NullInputException, WrongInputException, OptionUtils}
 import com.jabong.dap.common.constants.campaign.Recommendation
 import com.jabong.dap.common.time.TimeUtils
 import com.jabong.dap.data.acq.common.ParamInfo
@@ -14,10 +14,12 @@ object RecommendationGenerator extends Logging {
    * start the recommendation generation Process
    * @param paramInfo
    */
+  @throws(classOf[WrongInputException])
+  @throws(classOf[NullInputException])
   def start(paramInfo: ParamInfo) {
     val incrDate = OptionUtils.getOptValue(paramInfo.incrDate, TimeUtils.YESTERDAY_FOLDER)
     val pivotKey = OptionUtils.getOptValue(paramInfo.subType, Recommendation.BRICK_MVP_SUB_TYPE)
-    logger.info(("Recommendation Process has started for pivotkey:- %s  date:- %s\",pivotKey,incrDate"))
+    logger.info(("Recommendation Process has started for pivotkey:- %s  date:- %s",pivotKey,incrDate))
     RecommendationInput.loadCommonDataSets(incrDate)
     PivotRecommendation.generateRecommendation(RecommendationInput.orderItemFullData, RecommendationInput.lastdayItrData, pivotKey, Recommendation.NUM_RECOMMENDATIONS, incrDate)
     logger.info(("Recommendation successfully generated for pivotkey:- %s  date:- %s", pivotKey, incrDate))
