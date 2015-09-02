12b5daa1b5d6c85021b941a74a0392014442308b
Rahul Aneja <rahul.aneja@jabong.com>
2015-09-02 15:01:10 +0530

    added more test schema

--- core/src/test/scala/com/jabong/dap/campaign/recommendation/generate/CommonRecommendationTest.scala
+++ core/src/test/scala/com/jabong/dap/campaign/recommendation/generate/CommonRecommendationTest.scala
@@ -1,14 +1,17 @@
 package com.jabong.dap.campaign.recommendation.generate
 
-import com.jabong.dap.campaign.recommendation.generator.CommonRecommendation
+import com.jabong.dap.campaign.recommendation.generator.{PivotRecommendation, CommonRecommendation}
 import com.jabong.dap.common.constants.campaign.Recommendation
 import com.jabong.dap.common.constants.variables.ProductVariables
 import com.jabong.dap.common.json.JsonUtils
 import com.jabong.dap.common.time.{TimeConstants, TimeUtils}
 import com.jabong.dap.common.{NullInputException, SharedSparkContext, Spark, TestSchema}
 import com.jabong.dap.data.storage.DataSets
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.scalatest.{FlatSpec, Matchers}
+
+import scala.collection.mutable
 
 /**
  * Created by rahul aneja  on 28/8/15.
@@ -19,6 +22,7 @@ class CommonRecommendationTest extends FlatSpec with SharedSparkContext with Mat
   @transient var itrDataFrame: DataFrame = _
   @transient var orderItemDataFrame: DataFrame = _
   @transient var inventoryCheckInput: DataFrame = _
+  @transient var skuCompleteInput: DataFrame = _
   var commonRecommendation: CommonRecommendation = _
   var days: Int = _
 
@@ -26,9 +30,10 @@ class CommonRecommendationTest extends FlatSpec with SharedSparkContext with Mat
     super.beforeAll()
     sqlContext = Spark.getSqlContext()
     days = TimeUtils.daysFromToday(TimeUtils.getDate("2015-08-27", TimeConstants.DATE_FORMAT))
-    commonRecommendation = new CommonRecommendation()
+    commonRecommendation = PivotRecommendation
     orderItemDataFrame = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "sales_order_item_weekly_average_sales")
     inventoryCheckInput = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "inventory_check_input", TestSchema.inventoryCheckInput)
+    skuCompleteInput = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "sku_complete_data_input", TestSchema.skuCompleteInput)
     itrDataFrame = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "basic_sku_itr")
   }
 
@@ -88,22 +93,100 @@ class CommonRecommendationTest extends FlatSpec with SharedSparkContext with Mat
   }
 
   "top skus  input and itr" should "return sku complete data" in {
-    val recInput = commonRecommendation.skuCompleteData(commonRecommendation.topProductsSold(orderItemDataFrame), itrDataFrame)
-    val recInputbrand = recInput.filter(ProductVariables.SKU + "='ES418WA79UAUINDFAS'").select(ProductVariables.BRAND).collect()(0)(0).toString()
-    assert(recInputbrand == "adidas")
+    val recInput = commonRecommendation.skuCompleteData(skuCompleteInput, itrDataFrame)
+    val recInputBrand = recInput.filter(Recommendation.SALES_ORDER_ITEM_SKU + "='ES418WA79UAUINDFAS'").select(ProductVariables.BRAND).collect()(0)(0).toString()
+    assert(recInputBrand == "adidas")
   }
 
   " skus BR828MA28TMPINDFAS input and itr" should "return WOMEN GENDER" in {
-    val recInput = commonRecommendation.skuCompleteData(commonRecommendation.topProductsSold(orderItemDataFrame), itrDataFrame)
-    val recInputbrand = recInput.filter(ProductVariables.SKU + "='SO596WA65JLIINDFAS'").select(ProductVariables.GENDER).collect()(0)(0).toString()
-    assert(recInputbrand == "MEN")
+    val recInput = commonRecommendation.skuCompleteData(skuCompleteInput, itrDataFrame)
+    val recInputBrand = recInput.filter(Recommendation.SALES_ORDER_ITEM_SKU + "='SO596WA65JLIINDFAS'").select(ProductVariables.GENDER).collect()(0)(0).toString()
+    assert(recInputBrand == "WOMEN")
   }
 
-  "No top skus input and itr" should "return null data" in {
-    val recInput = commonRecommendation.skuCompleteData(null, itrDataFrame)
-    assert(recInput == null)
+  "No top skus input and itr" should "return IllegalArgumentException " in {
+    a[IllegalArgumentException] should be thrownBy {
+      commonRecommendation.skuCompleteData(null, itrDataFrame)
+    }
   }
 
+  "No Recommendation input skus" should "return IllegalArgumentException" in {
+    a[IllegalArgumentException] should be thrownBy {
+      commonRecommendation.genRecommend(null, null, null, 0)
+    }
+  }
+
+  "5 recommendation input skus and pivot keys is null" should " return IllegalArgumentException" in {
+    val dataFrameSchema = StructType(Array(
+      StructField(ProductVariables.BRICK, StringType, false),
+      StructField(ProductVariables.MVP, LongType, false),
+      StructField(ProductVariables.GENDER, StringType, false),
+      StructField(ProductVariables.RECOMMENDATIONS, ArrayType(StructType(Array(StructField(ProductVariables.QUANTITY, LongType), StructField(ProductVariables.SKU_LIST, StringType))), true))
+    ))
+    a[IllegalArgumentException] should be thrownBy {
+      commonRecommendation.genRecommend(inventoryCheckInput, null, dataFrameSchema, 8)
+    }
+  }
+
+  "5 recommendation input skus" should "create recommendation based on brick mvp and gender" in {
+    val dataFrameSchema = StructType(Array(
+      StructField(ProductVariables.BRICK, StringType, false),
+      StructField(ProductVariables.MVP, LongType, false),
+      StructField(ProductVariables.GENDER, StringType, false),
+      StructField(ProductVariables.RECOMMENDATIONS, ArrayType(StructType(Array(StructField(ProductVariables.QUANTITY, LongType), StructField(ProductVariables.SKU_LIST, StringType))), true))
+    ))
+    val pivotKeys = Array(ProductVariables.BRICK, ProductVariables.MVP)
+
+    val recOut = commonRecommendation.genRecommend(inventoryCheckInput, pivotKeys, dataFrameSchema,8)
+
+    val recommendations = recOut.filter(ProductVariables.GENDER + "='WOMEN'")
+      .select(ProductVariables.RECOMMENDATIONS).collect()(0)(0).asInstanceOf[mutable.MutableList[(Long, String)]]
+    assert(recommendations.length == 1)
+  }
+
+  "5 recommendation input skus" should "create recommendation based on brick,brand mvp and gender" in {
+    val dataFrameSchema = StructType(Array(
+      StructField(ProductVariables.BRICK, StringType, false),
+      StructField(ProductVariables.MVP, LongType, false),
+      StructField(ProductVariables.BRAND, StringType, false),
+      StructField(ProductVariables.GENDER, StringType, false),
+      StructField(ProductVariables.RECOMMENDATIONS, ArrayType(StructType(Array(StructField(ProductVariables.QUANTITY, LongType), StructField(ProductVariables.SKU_LIST, StringType))), true))
+    ))
+    val pivotKeys = Array(ProductVariables.BRICK, ProductVariables.MVP, ProductVariables.BRAND)
+
+    val recOut = commonRecommendation.genRecommend(inventoryCheckInput, pivotKeys, dataFrameSchema,8)
+
+    val recommendations = recOut.filter(ProductVariables.GENDER + "='UNISEX'")
+      .select(ProductVariables.RECOMMENDATIONS).collect()(0)(0).asInstanceOf[mutable.MutableList[(Long, String)]]
+    assert(recommendations.length == 1)
+  }
+
+  "Given a row and array of keys" should "create a dynamic row with those keys" in {
+  val keys = Array(ProductVariables.BRICK, ProductVariables.MVP)
+  val expectedRow: Seq[Any] = Seq("test", "mass")
+  val row = commonRecommendation.createKey(inventoryCheckInput.head(), keys)
+    row.toSeq.foreach(println)
+  assert((row.toSeq).equals(expectedRow))
+}
+
+"Given a null row and array of keys" should "return null row" in {
+val keys = Array(ProductVariables.BRICK, ProductVariables.MVP)
+val row = commonRecommendation.createKey(null, keys)
+assert(row == null)
+}
+
+"Given a  row and  no keys" should "return null row" in {
+val keys: Array[String] = Array()
+val row = commonRecommendation.createKey(inventoryCheckInput.head(), keys)
+assert(row == null)
+}
+
+"Given a row and array of keys with one Bad key" should "null row" in {
+val keys = Array(ProductVariables.BRICK, "Bad")
+val expectedRow: Seq[Any] = Seq("test", "mass")
+val row = commonRecommendation.createKey(inventoryCheckInput.head(), keys)
+assert(row == null)
+}
 
 }
 
