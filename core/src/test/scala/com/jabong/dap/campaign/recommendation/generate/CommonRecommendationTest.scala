8ac8212785d93c6b515c469a8c000a1e6ff4c3bf
Rahul Aneja <rahul.aneja@jabong.com>
2015-09-04 10:20:59 +0530

    added brand mvp schema

--- core/src/test/scala/com/jabong/dap/campaign/recommendation/generate/CommonRecommendationTest.scala
+++ core/src/test/scala/com/jabong/dap/campaign/recommendation/generate/CommonRecommendationTest.scala
@@ -5,10 +5,11 @@ import com.jabong.dap.common.constants.campaign.Recommendation
 import com.jabong.dap.common.constants.variables.ProductVariables
 import com.jabong.dap.common.json.JsonUtils
 import com.jabong.dap.common.time.{ TimeConstants, TimeUtils }
-import com.jabong.dap.common.{ NullInputException, SharedSparkContext, Spark, TestSchema }
+import com.jabong.dap.common._
 import com.jabong.dap.data.storage.DataSets
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.{ DataFrame, SQLContext }
+import org.apache.spark.sql.{Row, DataFrame, SQLContext}
 import org.scalatest.{ FlatSpec, Matchers }
 
 import scala.collection.mutable
@@ -16,13 +17,14 @@ import scala.collection.mutable
 /**
  * Created by rahul aneja  on 28/8/15.
  */
-class CommonRecommendationTest extends FlatSpec with SharedSparkContext with Matchers {
+class CommonRecommendationTest extends FlatSpec with SharedSparkContext with Matchers  {
 
   @transient var sqlContext: SQLContext = _
   @transient var itrDataFrame: DataFrame = _
   @transient var orderItemDataFrame: DataFrame = _
   @transient var inventoryCheckInput: DataFrame = _
   @transient var skuCompleteInput: DataFrame = _
+  @transient var generateRecommendedSkuInput: DataFrame = _
   var commonRecommendation: CommonRecommendation = _
   var days: Int = _
 
@@ -34,6 +36,7 @@ class CommonRecommendationTest extends FlatSpec with SharedSparkContext with Mat
     orderItemDataFrame = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "sales_order_item_weekly_average_sales")
     inventoryCheckInput = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "inventory_check_input", TestSchema.inventoryCheckInput)
     skuCompleteInput = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "sku_complete_data_input", TestSchema.skuCompleteInput)
+    generateRecommendedSkuInput = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "recommendation_sku_input", TestSchema.recommendationSku)
     itrDataFrame = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "basic_sku_itr")
   }
 
@@ -56,7 +59,7 @@ class CommonRecommendationTest extends FlatSpec with SharedSparkContext with Mat
 
   "last seven days with weekly average sales and 30 days order item dataframe" should "adding weekly average 30 days " in {
     val expectedValue = commonRecommendation.addWeeklyAverageSales(commonRecommendation.createWeeklyAverageSales(orderItemDataFrame),
-      commonRecommendation.topProductsSold(orderItemDataFrame))
+      commonRecommendation.productsWithCountSold(orderItemDataFrame))
     val expectedSku = expectedValue.filter(Recommendation.SALES_ORDER_ITEM_SKU + " = 'ES418WA79UAUINDFAS'")
     assert(expectedSku.count() == 1)
   }
@@ -64,7 +67,7 @@ class CommonRecommendationTest extends FlatSpec with SharedSparkContext with Mat
   "null weekly average sales and 30 days order item dataframe" should " return NullInputException " in {
     a[NullInputException] should be thrownBy {
       commonRecommendation.addWeeklyAverageSales(null,
-        commonRecommendation.topProductsSold(orderItemDataFrame))
+        commonRecommendation.productsWithCountSold(orderItemDataFrame))
     }
   }
 
@@ -82,12 +85,12 @@ class CommonRecommendationTest extends FlatSpec with SharedSparkContext with Mat
 
   "Null order data" should "return null dataframe" in {
     a[NullInputException] should be thrownBy {
-      commonRecommendation.topProductsSold(null)
+      commonRecommendation.productsWithCountSold(null)
     }
   }
 
   "Order Items data input  " should "return sku and sorted by quantity dataframe" in {
-    val topSkus = commonRecommendation.topProductsSold(orderItemDataFrame)
+    val topSkus = commonRecommendation.productsWithCountSold(orderItemDataFrame)
     assert(topSkus.count() == 2)
   }
 
@@ -185,6 +188,42 @@ class CommonRecommendationTest extends FlatSpec with SharedSparkContext with Mat
     val expectedRow: Seq[Any] = Seq("test", "mass")
     val row = commonRecommendation.createKey(inventoryCheckInput.head(), keys)
     assert(row == null)
+  }
+
+
+  "Given iterable array as null" should "illegal arguument exception" in {
+    a[IllegalArgumentException] should be thrownBy {
+      commonRecommendation.genSku(null)
+    }
+  }
+
+  "Given iterable array as empty " should "illegal argument exception" in {
+    val iterableInput:mutable.Iterable[Row] = mutable.Iterable()
+    a[IllegalArgumentException] should be thrownBy {
+      commonRecommendation.genSku(iterableInput)
+    }
+  }
+
+  "Given iterable array for case 1  " should "return key value pairs" in {
+    val recommendationOutput = generateRecommendedSkuInput.filter(TestConstants.TEST_CASE_FILTER+"= 1").map(row => ((row(row.fieldIndex(ProductVariables.BRICK)),row(row.fieldIndex(ProductVariables.MVP))),row))
+      .groupByKey()
+    val recOut = commonRecommendation.genSku(recommendationOutput.first()._2).get("WOMEN")
+    val recSku = recOut.get(0)._2
+    assert(recSku == "ES418WA79UAUINDFAS")
+    assert(recOut.get.count(_._1!=0) == 2)
+  }
+
+  "Given iterable array for case 2 " should "return sku for BOYS gender SO596WA65JLIINDFAS and for BLANK ES418WA79UAUINDFAS if the gender is not valid " in {
+    val recommendationOutput = generateRecommendedSkuInput.filter(TestConstants.TEST_CASE_FILTER+"= 2").map(row => ((row(row.fieldIndex(ProductVariables.BRICK)),row(row.fieldIndex(ProductVariables.MVP))),row))
+      .groupByKey()
+    val recOut = commonRecommendation.genSku(recommendationOutput.take(2)(1)._2).get("BOYS")
+    val recOut1 = commonRecommendation.genSku(recommendationOutput.take(2)(1)._2).get("BLANK")
+    val recSku = recOut.get(0)._2
+    val recSku1 = recOut1.get(0)._2
+    assert(recSku == "SO596WA65JLIINDFAS")
+    assert(recSku1 == "ES418WA79UAUINDFAS")
+    assert(recOut.get.count(_._1!=0) == 1)
+    assert(recOut1.get.count(_._1!=0) == 1)
   }
 
 }
