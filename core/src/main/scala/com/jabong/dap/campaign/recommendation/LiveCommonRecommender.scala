91a89c59d68c9141dbeefe17ea2676e45cebacb7
Rahul Aneja <rahul.aneja@jabong.com>
2015-09-07 14:39:34 +0530

    live common recommender

--- core/src/main/scala/com/jabong/dap/campaign/recommendation/LiveCommonRecommender.scala
+++ core/src/main/scala/com/jabong/dap/campaign/recommendation/LiveCommonRecommender.scala
@@ -1,7 +1,8 @@
 package com.jabong.dap.campaign.recommendation
 
+import com.jabong.dap.common.constants.campaign.{Recommendation, CampaignMergedFields}
 import com.jabong.dap.common.constants.config.ConfigConstants
-import com.jabong.dap.common.constants.variables.ProductVariables
+import com.jabong.dap.common.constants.variables.{CustomerVariables, ProductVariables}
 import com.jabong.dap.common.time.TimeUtils
 import com.jabong.dap.data.storage.DataSets
 import com.jabong.dap.data.write.DataWriter
@@ -16,10 +17,41 @@ class LiveCommonRecommender extends Recommender with Logging {
   /**
    * Place holder function which will get recommended skus
    * @param refSkus
-   * @param yesterdayItr
+   * @param recommendations
    * @return
    */
-  override def generateRecommendation(refSkus: DataFrame, yesterdayItr: DataFrame): DataFrame = {
-    return null
+  override def generateRecommendation(refSkus: DataFrame, recommendations: DataFrame): DataFrame = {
+    require(refSkus!=null,"refSkus cannot be null")
+    require(recommendations!=null,"recommendations cannot be null")
+
+    val recommendationJoined = refSkus.join(recommendations,refSkus(ProductVariables.BRICK) === recommendations(ProductVariables.BRICK)
+      && refSkus(ProductVariables.MVP) === recommendations(ProductVariables.MVP)
+      && refSkus(ProductVariables.GENDER) === recommendations(ProductVariables.GENDER))
+      .select(refSkus(CampaignMergedFields.REF_SKU1),
+              refSkus(CampaignMergedFields.CAMPAIGN_MAIL_TYPE),
+              refSkus(CustomerVariables.FK_CUSTOMER),
+              recommendations(ProductVariables.BRICK),
+              recommendations(ProductVariables.MVP),
+              recommendations(ProductVariables.GENDER),
+              recommendations(ProductVariables.RECOMMENDATIONS))
+
+  //  val refSkusWithRecommendations = recommendationJoined.map(row=> getRecommendedSkus(row(row.fieldIndex(CampaignMergedFields.REF_SKU1))
+    //  ,row(row.fieldIndex(ProductVariables.RECOMMENDATIONS))))
+
+
+    return recommendationJoined
+  }
+
+  /**
+   *
+   * @param refSku
+   * @param recommendation
+   * @return
+   */
+  def getRecommendedSkus(refSku :String, recommendation: List[(Long,String)]):List[String] ={
+    require(refSku!=null,"refSkus cannot be null")
+    require(recommendation!=null,"recommendation cannot be null")
+    val outputSkus = recommendation.filterNot(_._2==refSku).take(8).map(_._2)
+    return outputSkus
   }
 }
