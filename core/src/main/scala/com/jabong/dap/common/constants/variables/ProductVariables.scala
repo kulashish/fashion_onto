6b078fe59b6f00c01758109775bf49f7c7681e3f
Rahul Aneja <rahul.aneja@jabong.com>
2015-09-11 11:51:03 +0530

    removed constant in product variables

--- core/src/main/scala/com/jabong/dap/common/constants/variables/ProductVariables.scala
+++ core/src/main/scala/com/jabong/dap/common/constants/variables/ProductVariables.scala
@@ -14,6 +14,5 @@ object ProductVariables {
   val CATEGORY = "category"
   val SKU_LIST = "skuList"
   val QUANTITY = "quantity"
-  val RECOMMENDATIONS = "recommendations"
   val NUMBER_SIMPLE_PER_SKU = "number_simple_per_sku"
 }
