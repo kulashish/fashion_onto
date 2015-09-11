87e675eb9e28c0dd8e793021935bf560a6599e42
Rahul Aneja <rahul.aneja@jabong.com>
2015-09-11 11:51:42 +0530

    added test cases for live common recommender

--- core/src/test/scala/com/jabong/dap/campaign/recommendation/LiveCommonRecommenderTest.scala
+++ core/src/test/scala/com/jabong/dap/campaign/recommendation/LiveCommonRecommenderTest.scala
@@ -1,10 +1,11 @@
 package com.jabong.dap.campaign.recommendation
 
+import com.jabong.dap.common.constants.variables.{CustomerVariables, ProductVariables}
 import com.jabong.dap.common.json.JsonUtils
-import com.jabong.dap.common.{ SharedSparkContext, Spark, TestSchema }
+import com.jabong.dap.common.{TestConstants, SharedSparkContext, Spark, TestSchema}
 import com.jabong.dap.data.storage.DataSets
 import com.jabong.dap.data.storage.schema.Schema
-import org.apache.spark.sql.{ DataFrame, SQLContext }
+import org.apache.spark.sql.{Row, DataFrame, SQLContext}
 import org.scalatest.{ Matchers, FlatSpec }
 
 /**
@@ -15,6 +16,9 @@ class LiveCommonRecommenderTest extends FlatSpec with SharedSparkContext with Ma
   @transient var sqlContext: SQLContext = _
   @transient var recommendationOutput: DataFrame = _
   @transient var refSkusData: DataFrame = _
+  @transient var newReferenceSkuData: DataFrame = _
+  @transient var genRecSkuInput: DataFrame = _
+
   var liveRecommender: LiveCommonRecommender = _
 
   override def beforeAll() {
@@ -22,6 +26,8 @@ class LiveCommonRecommenderTest extends FlatSpec with SharedSparkContext with Ma
     sqlContext = Spark.getSqlContext()
     liveRecommender = new LiveCommonRecommender()
     recommendationOutput = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "brick_mvp_recommendation_output", Schema.brickMvpRecommendationOutput)
+    newReferenceSkuData = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "ref_sku_data1", TestSchema.finalReferenceSku)
+    genRecSkuInput = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "gen_rec_sku_input", TestSchema.genRecInput)
     refSkusData = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/recommendation/", "ref_sku_data", TestSchema.referenceSku)
   }
 
@@ -33,19 +39,20 @@ class LiveCommonRecommenderTest extends FlatSpec with SharedSparkContext with Ma
 
   "null recommendations  data" should "return IllegalArgumentException" in {
     a[IllegalArgumentException] should be thrownBy {
-      liveRecommender.generateRecommendation(refSkusData, null)
+      liveRecommender.generateRecommendation(newReferenceSkuData, null)
     }
   }
 
   "refskus and brick mvp geneder recommendation  data as input" should "return set of eight recommendedSkus for refSkus" in {
-    val expectedRecommendations = liveRecommender.generateRecommendation(refSkusData, recommendationOutput)
+    val expectedRecommendations = liveRecommender.generateRecommendation(newReferenceSkuData, recommendationOutput)
     //println("RECOMMENDED"+expectedRecommendations.count())
     //expectedRecommendations.collect().foreach(println)
     assert(expectedRecommendations != null)
   }
 
   "refSku is null" should "return IllegalArgumentException" in {
-    val inputRecommendations: List[(Long, String)] = List((7L, "SO596WA65JLIINDFAS"), (4L, "AD004WA32UCLINDFAS"))
+    val inputRecommendations: List[Row] = List(Row(7L, "SO596WA65JLIINDFAS"), Row(4L, "ES418WA79UAUINDFAS"))
+
     a[IllegalArgumentException] should be thrownBy {
       liveRecommender.getRecommendedSkus(null, inputRecommendations)
     }
@@ -57,12 +64,60 @@ class LiveCommonRecommenderTest extends FlatSpec with SharedSparkContext with Ma
       liveRecommender.getRecommendedSkus(refSku, null)
     }
   }
+
   "ref sku AD004WA32UCLINDFAS with two recommendation " should "return ref skus with recommendations" in {
     val refSku = "AD004WA32UCLINDFAS"
-    val inputRecommendations: List[(Long, String)] = List((7L, "SO596WA65JLIINDFAS"), (4L, "ES418WA79UAUINDFAS"))
+    val row1 = Row(7L, "SO596WA65JLIINDFAS")
+    val row2 = Row(4L, "ES418WA79UAUINDFAS")
+    val inputRecommendations: List[Row] = List(row1, row2)
     val expectedValue = liveRecommender.getRecommendedSkus(refSku, inputRecommendations)
-    expectedValue.foreach(println)
-    assert(expectedValue != null)
+    assert(expectedValue.size == 2)
+  }
+
+  "ref sku SO596WA65JLIINDFAS with two recommendation " should "return ref skus with recommendations" in {
+    val refSku = "SO596WA65JLIINDFAS"
+    val row1 = Row(7L, "SO596WA65JLIINDFAS")
+    val row2 = Row(4L, "ES418WA79UAUINDFAS")
+    val inputRecommendations: List[Row] = List(row1, row2)
+    val expectedValue = liveRecommender.getRecommendedSkus(refSku, inputRecommendations)
+    assert(expectedValue.size == 1)
+  }
+
+  "refskus and brick mvp geneder recommendation  data as input" should "return ref skus with max eight recommendations" in {
+    val expectedValue = liveRecommender.generateRecommendation(newReferenceSkuData, recommendationOutput)
+    assert(expectedValue.count == 4)
+  }
+
+  "iterable row is null " should "return IllegalArgumentException" in {
+    a[IllegalArgumentException] should be thrownBy {
+      liveRecommender.getRecSkus(null)
+    }
+  }
+
+
+  "one ref sku per customer and only 3 recommended skus " should "return max 3 rec skus for the same ref sku" in {
+    val refRecSkuInput = genRecSkuInput.filter(TestConstants.TEST_CASE_FILTER + "= 1").map(row => ((row(row.fieldIndex(CustomerVariables.FK_CUSTOMER))), row))
+      .groupByKey()
+
+    val expectedOut = liveRecommender.getRecSkus(refRecSkuInput.first()._2)
+    assert(expectedOut._2.size ==3)
+  }
+
+
+  "one ref sku per customer  and more than 8" should "return max 8 rec skus for the same ref sku" in {
+    val refRecSkuInput = genRecSkuInput.filter(TestConstants.TEST_CASE_FILTER + "= 3").map(row => ((row(row.fieldIndex(CustomerVariables.FK_CUSTOMER))), row))
+      .groupByKey()
+
+    val expectedOut = liveRecommender.getRecSkus(refRecSkuInput.first()._2)
+    assert(expectedOut._2.size ==8)
+  }
+
+  "two ref sku per customer  and more than 8 in total" should "return max 8 rec skus and no duplicates for the same ref sku" in {
+    val refRecSkuInput = genRecSkuInput.filter(TestConstants.TEST_CASE_FILTER + "= 2").map(row => ((row(row.fieldIndex(CustomerVariables.FK_CUSTOMER))), row))
+      .groupByKey()
+
+    val expectedOut = liveRecommender.getRecSkus(refRecSkuInput.first()._2)
+    assert(expectedOut._2.size ==8)
   }
 
 }
