4232137e02444c3f638a614347608e368b585787
Rahul Aneja <rahul.aneja@jabong.com>
2015-09-14 14:32:39 +0530

    changed cancel and retarget campaigns for email campaigns

--- core/src/main/scala/com/jabong/dap/campaign/campaignlist/LiveCancelReTargetCampaign.scala
+++ core/src/main/scala/com/jabong/dap/campaign/campaignlist/LiveCancelReTargetCampaign.scala
@@ -4,7 +4,7 @@ import com.jabong.dap.campaign.data.CampaignOutput
 import com.jabong.dap.campaign.manager.CampaignProducer
 import com.jabong.dap.campaign.skuselection.CancelReTarget
 import com.jabong.dap.campaign.utils.CampaignUtils
-import com.jabong.dap.common.constants.campaign.{ CampaignCommon, SkuSelection }
+import com.jabong.dap.common.constants.campaign.{Recommendation, CampaignCommon, SkuSelection}
 import org.apache.spark.sql.DataFrame
 
 /**
@@ -12,20 +12,27 @@ import org.apache.spark.sql.DataFrame
  */
 class LiveCancelReTargetCampaign {
 
-  def runCampaign(targetCustomersWithOrderItems: DataFrame): Unit = {
+  def runCampaign(targetCustomersWithOrderItems: DataFrame, yesterdayItrData: DataFrame, brickMvpRecommendations: DataFrame): Unit = {
     // targetCustomersWithOrderItems = (id_customer, id_sales_order, item_status, unit_price, updated_at, sku_simple)
 
     // filter only by return status
     // FIXME: filter by campaign -- i.e., campaign shount not have gone to customer in last 30 days
     // find list of (customers, ref skus)
-    val refSkus = CancelReTarget.skuFilter(targetCustomersWithOrderItems)
+    val filteredSkus = CancelReTarget.skuFilter(targetCustomersWithOrderItems)
 
-    // create recommendations
-    //  val recommender = CampaignProducer.getFactory(CampaignCommon.RECOMMENDER).getRecommender("Null")
-    // val recommendations = recommender.recommend(refSkus)
 
     // save 2 ref skus + 8 recommendation per customer (null allowed for mobile push)
-    val campaignOutput = CampaignUtils.addCampaignMailType(refSkus, CampaignCommon.CANCEL_RETARGET_CAMPAIGN)
+    val filteredSkuJoinedItr = CampaignUtils.yesterdayItrJoin(filteredSkus,yesterdayItrData)
+
+    val refSkus = CampaignUtils.generateReferenceSkus(filteredSkuJoinedItr, CampaignCommon.NUMBER_REF_SKUS)
+
+    val refSkusWithCampaignId = CampaignUtils.addCampaignMailType(refSkus, CampaignCommon.CANCEL_RETARGET_CAMPAIGN)
+
+
+    // create recommendations
+    val recommender = CampaignProducer.getFactory(CampaignCommon.RECOMMENDER).getRecommender(Recommendation.LIVE_COMMON_RECOMMENDER)
+
+     val campaignOutput = recommender.generateRecommendation(refSkusWithCampaignId,brickMvpRecommendations)
 
     //save campaign Output
     CampaignOutput.saveCampaignDataForYesterday(campaignOutput, CampaignCommon.CANCEL_RETARGET_CAMPAIGN)
