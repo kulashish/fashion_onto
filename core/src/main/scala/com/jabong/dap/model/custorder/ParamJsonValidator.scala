48868c0cb38d699a229635f7543b1b4d1558a28e
Tejas Jain <tejas.jain@jabong.com>
2015-09-14 12:29:39 +0530

    Changes in ParamJsonValidator.scala

--- core/src/main/scala/com/jabong/dap/model/custorder/ParamJsonValidator.scala
+++ core/src/main/scala/com/jabong/dap/model/custorder/ParamJsonValidator.scala
@@ -1,8 +1,7 @@
 package com.jabong.dap.model.custorder
 
-import com.jabong.dap.data.acq.common.{ ParamJobInfo, ParamInfo }
+import com.jabong.dap.data.acq.common.{ParamInfo, ParamJobInfo}
 import com.jabong.dap.data.storage.DataSets
-import com.jabong.dap.model.clickstream.ClickStreamConstant
 
 
 /**
@@ -17,7 +16,7 @@ object ParamJsonValidator {
 
   private def validatePossibleValues(paramInfo: ParamInfo) = {
     val possibleSourceFormats = Array(DataSets.AD4PUSH_CUSTOMER_RESPONSE, DataSets.CUSTOMER_DEVICE_MAPPING, DataSets.BASIC_ITR,
-      DataSets.PRICING, DataSets.DCF_FEED, DataSets.CAMPAIGN_QUALITY, DataSets.AD4PUSH_DEVICE_MERGER,ClickStreamConstant.CLICKSTREAM_DATA_QUALITY)
+      DataSets.PRICING, DataSets.DCF_FEED, DataSets.CAMPAIGN_QUALITY, DataSets.AD4PUSH_DEVICE_MERGER,DataSets.CLICKSTREAM_DATA_QUALITY)
     val possibleSaveFormats = Array(DataSets.ORC, DataSets.PARQUET)
     val possibleSaveModes = Array(DataSets.OVERWRITE_SAVEMODE, DataSets.APPEND_SAVEMODE, DataSets.IGNORE_SAVEMODE, DataSets.ERROR_SAVEMODE)
 
