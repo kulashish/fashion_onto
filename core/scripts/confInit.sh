8c13b7ff4dcd313e54fe59e57710a575cbb9bee8
Pooja Jain <pooja.jain@jabong.com>
2015-09-15 14:03:53 +0530

    fixes for oozie workflow

--- core/scripts/confInit.sh
+++ core/scripts/confInit.sh
@@ -2,10 +2,17 @@
 
 # copy config in a hdfs
 hadoop fs -rm -r /apps/alchemy/conf/*.json
-hadoop fs -put /opt/alchemy-core/current/conf/*.json /apps/alchemy/conf/
 hadoop fs -put /opt/alchemy-core/base/config.json /apps/alchemy/conf/
+hadoop fs -put /opt/alchemy-core/current/conf/*.json /apps/alchemy/conf/
+
+# copy workflow files to hdfs
 hadoop fs -rm -r /apps/alchemy/workflows/*
 hadoop fs -put /opt/alchemy-core/current/workflows/* /apps/alchemy/workflows/
+
+# copying lib to hdfs
 hadoop fs -mkdir -p /apps/alchemy/workflows/lib/
 hadoop fs -put /opt/alchemy-core/current/jar/Alchemy-assembly.jar /apps/alchemy/workflows/lib/.
 
+#copying run.pl to hdfs
+hadoop fs -mkdir -p /apps/alchemy/workflows/scripts/
+hadoop fs -put /opt/alchemy-core/current/bin/run.pl /apps/alchemy/workflows/scripts/.
