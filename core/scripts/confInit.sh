3d893b5995c64686166a1da5397c311809ad8d41
Pooja Jain <pooja.jain@jabong.com>
2015-09-15 12:38:05 +0530

    changes to copy the jar file to workflows dir

--- core/scripts/confInit.sh
+++ core/scripts/confInit.sh
@@ -6,4 +6,6 @@ hadoop fs -put /opt/alchemy-core/current/conf/*.json /apps/alchemy/conf/
 hadoop fs -put /opt/alchemy-core/base/config.json /apps/alchemy/conf/
 hadoop fs -rm -r /apps/alchemy/workflows/*
 hadoop fs -put /opt/alchemy-core/current/workflows/* /apps/alchemy/workflows/
+hadoop fs -mkdir -p /apps/alchemy/workflows/lib/
+hadoop fs -put /opt/alchemy-core/current/jar/Alchemy-assembly.jar /apps/alchemy/workflows/lib/.
 
