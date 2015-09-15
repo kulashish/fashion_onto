00ee7d25f9abea24081c00f5c9d0ec3091a43929
Pooja Jain <pooja.jain@jabong.com>
2015-09-15 11:49:25 +0530

    added changes for running oozie workflow using perl scripts

--- core/scripts/confInit.sh
+++ core/scripts/confInit.sh
@@ -4,5 +4,6 @@
 hadoop fs -rm -r /apps/alchemy/conf/*.json
 hadoop fs -put /opt/alchemy-core/current/conf/*.json /apps/alchemy/conf/
 hadoop fs -put /opt/alchemy-core/base/config.json /apps/alchemy/conf/
-hadoop fs -rm -r /apps/alchemy/
+hadoop fs -rm -r /apps/alchemy/workflows/*
+hadoop fs -put /opt/alchemy-core/current/workflows/* /apps/alchemy/workflows/
 
