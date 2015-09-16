1936b9c5972e5a4c9c96f93f4e4874565fab7c6e
Kapil Rajak <caps.raaj@gmail.com>
2015-09-16 14:24:46 +0530

    invalidIODCampaign

---
+++ core/src/main/scala/com/jabong/dap/campaign/campaignlist/InvalidIODCampaign.scala
@@ -0,0 +1,38 @@
+package com.jabong.dap.campaign.campaignlist
+
+import com.jabong.dap.campaign.data.CampaignOutput
+import com.jabong.dap.campaign.manager.CampaignProducer
+import com.jabong.dap.campaign.skuselection.ItemOnDiscount
+import com.jabong.dap.campaign.traceability.PastCampaignCheck
+import com.jabong.dap.campaign.utils.CampaignUtils
+import com.jabong.dap.common.constants.campaign.{SkuSelection, CustomerSelection, CampaignCommon}
+import org.apache.spark.sql.DataFrame
+
+/**
+ * Created by kapil on 9/9/15.
+ */
+class InvalidIODCampaign {
+
+  def runCampaign(past30DayCampaignMergedData: DataFrame,customerOrderData: DataFrame, orderItemData: DataFrame,last30daysItrData: DataFrame): Unit ={
+    val invalidCustomerSelector = CampaignProducer.getFactory(CampaignCommon.CUSTOMER_SELECTOR)
+      .getCustomerSelector(CustomerSelection.INVALID)
+    val selectedCustomers = invalidCustomerSelector.customerSelection(customerOrderData, orderItemData)
+
+    var custFiltered = selectedCustomers
+
+    if (past30DayCampaignMergedData != null) {
+      //past campaign check whether the campaign has been sent to customer in last 30 days
+      val pastCampaignCheck = new PastCampaignCheck()
+      custFiltered = pastCampaignCheck.campaignRefSkuCheck(past30DayCampaignMergedData, selectedCustomers,
+        CampaignCommon.campaignMailTypeMap.getOrElse(CampaignCommon.INVALID_LOWSTOCK_CAMPAIGN, 1000), 30)
+
+    }
+
+    //sku selection
+    val invalidIod = ItemOnDiscount.skuFilter(custFiltered,last30daysItrData)
+    val campaignOutput = CampaignUtils.addCampaignMailType(invalidIod, CampaignCommon.INVALID_IOD_CAMPAIGN)
+
+    //save campaign Output
+    CampaignOutput.saveCampaignDataForYesterday(campaignOutput, CampaignCommon.INVALID_IOD_CAMPAIGN)
+  }
+}
