e104c008fcdc4d37da557e0e1bf7108f70424094
Rahul Aneja <rahul.aneja@jabong.com>
2015-09-18 12:42:58 +0530

    added feature test cases and files

---
+++ core/src/test/scala/com/jabong/dap/campaign/campaignlist/LiveRetargetCampaignTest.scala
@@ -0,0 +1,39 @@
+package com.jabong.dap.campaign.campaignlist
+
+import com.jabong.dap.campaign.data.CampaignOutput
+import com.jabong.dap.common.{TestSchema, SharedSparkContext, Spark}
+import com.jabong.dap.common.json.JsonUtils
+import com.jabong.dap.data.storage.DataSets
+import com.jabong.dap.data.storage.schema.Schema
+import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.scalatest.{GivenWhenThen, FeatureSpec}
+
+/**
+ * Created by rahul on 18/9/15.
+ */
+class LiveRetargetCampaignTest extends FeatureSpec with GivenWhenThen with SharedSparkContext {
+
+  @transient var sqlContext: SQLContext = _
+  @transient var salesOrderItemData: DataFrame = _
+  @transient var salesOrderData: DataFrame = _
+  @transient var recommendationsData: DataFrame = _
+  @transient var yesterdayItrData: DataFrame = _
+
+  override def beforeAll() {
+    super.beforeAll()
+    CampaignOutput.setTestMode(true)
+    sqlContext = Spark.getSqlContext()
+    salesOrderItemData = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/email_campaigns", "sales_item", Schema.salesOrderItem)
+    salesOrderData = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/email_campaigns", "sales_order", Schema.salesOrder)
+    yesterdayItrData = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/email_campaigns", "sku_simple_common_itr", TestSchema.basicSimpleItr)
+    recommendationsData = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/email_campaigns", "brick_mvp_recommendations")
+  }
+
+  feature("Run live cancel retarget campaign") {
+    scenario("Order which has been cancelled ") {
+      Given("salesOrder, salesOrderItemData, yesterdayItrData, brickMvpRecommendation")
+      val liveRetargetCampaign = new LiveRetargetCampaign()
+      liveRetargetCampaign.runCampaign(salesOrderData, salesOrderItemData, yesterdayItrData, recommendationsData)
+    }
+  }
+}
