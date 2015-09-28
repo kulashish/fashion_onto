f27ae3f8be6419135a4415709532c8107734ac17
Pooja Jain <pooja.jain@jabong.com>
2015-09-28 17:27:24 +0530

    added cron

--- core/scripts/emailCampaignVariables.sh
+++ core/scripts/emailCampaignVariables.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
+
+# call custWelcomeVoucher
+perl /opt/alchemy-core/current/bin/run.pl -t PROD -c custWelcomeVoucher
+
+# ftp upload
 perl /opt/alchemy-core/current/bin/ftp_upload.pl -c custWelcomeVoucher
+
+# call custPreference
+perl /opt/alchemy-core/current/bin/run.pl -t PROD -c custPreference
+
+# ftp upload
 perl /opt/alchemy-core/current/bin/ftp_upload.pl -c custPreference
