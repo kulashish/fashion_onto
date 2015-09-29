252703b7aad589c2dc5091c562dae51b4ebe8e6e
Rahul Aneja <rahul.aneja@jabong.com>
2015-09-29 13:49:38 +0530

    re-format the code

--- core/src/main/scala/com/jabong/dap/campaign/recommendation/LiveCommonRecommender.scala
+++ core/src/main/scala/com/jabong/dap/campaign/recommendation/LiveCommonRecommender.scala
@@ -106,13 +106,13 @@ class LiveCommonRecommender extends Recommender with Logging {
    * @param iterable
    * @return
    */
-  def getRecSkus(iterable: Iterable[Row]): (mutable.MutableList[(String,String,String,String)], mutable.MutableList[String], Int, String) = {
+  def getRecSkus(iterable: Iterable[Row]): (mutable.MutableList[(String, String, String, String)], mutable.MutableList[String], Int, String) = {
     require(iterable != null, "iterable cannot be null")
     require(iterable.size != 0, "iterable cannot be of size zero")
 
     val topRow = iterable.head
     val recommendedSkus: mutable.MutableList[String] = mutable.MutableList()
-    val referenceSkus: mutable.MutableList[(String,String,String,String)] = mutable.MutableList()
+    val referenceSkus: mutable.MutableList[(String, String, String, String)] = mutable.MutableList()
     val recommendationIndex = topRow.fieldIndex(CampaignMergedFields.REC_SKUS)
     val campaignMailTypeIndex = topRow.fieldIndex(CampaignMergedFields.CAMPAIGN_MAIL_TYPE)
     val acartUrlIndex = topRow.fieldIndex(CampaignMergedFields.LIVE_CART_URL)
@@ -129,8 +129,7 @@ class LiveCommonRecommender extends Recommender with Logging {
       val recommendations = row(recommendationIndex).asInstanceOf[scala.collection.mutable.ArrayBuffer[String]].
         foreach(value => if (!recommendedSkus.contains(value) && i <= skuPerIteration) { recommendedSkus += value; i = i + 1; })
 
-      referenceSkus += ((row(refSkuIndex).toString ,CampaignUtils.checkNullString(row(liveBrandIndex))
-        ,CampaignUtils.checkNullString(row(liveBrickIndex)),CampaignUtils.checkNullString(row(liveProdNameIndex))))
+      referenceSkus += ((row(refSkuIndex).toString, CampaignUtils.checkNullString(row(liveBrandIndex)), CampaignUtils.checkNullString(row(liveBrickIndex)), CampaignUtils.checkNullString(row(liveProdNameIndex))))
 
     }
     return (referenceSkus, recommendedSkus, mailType, acartUrl)
