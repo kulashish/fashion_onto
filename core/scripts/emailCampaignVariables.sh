d30045746ba1632819b04b6d740abae888b6ab0f
Pooja Jain <pooja.jain@jabong.com>
2015-10-01 13:42:08 +0530

    corrected the path for variables

--- core/scripts/emailCampaignVariables.sh
+++ core/scripts/emailCampaignVariables.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 # call custWelcomeVoucher
-perl /opt/alchemy-core/current/bin/run.pl -t PROD -c custWelcomeVoucher
+perl /opt/alchemy-core/current/bin/run.pl -t TEST-PROD -c custWelcomeVoucher
 
 # ftp upload
 perl /opt/alchemy-core/current/bin/ftp_upload.pl -c custWelcomeVoucher
 
 # call custPreference
-perl /opt/alchemy-core/current/bin/run.pl -t PROD -c custPreference
+perl /opt/alchemy-core/current/bin/run.pl -t TEST-PROD -c custPreference
 
 # ftp upload
 perl /opt/alchemy-core/current/bin/ftp_upload.pl -c custPreference
 
 # call contactListMobile
-perl /opt/alchemy-core/current/bin/run.pl -t PROD -c contactListMobile
+perl /opt/alchemy-core/current/bin/run.pl -t TEST-PROD -c contactListMobile
 
 # ftp upload
 perl /opt/alchemy-core/current/bin/ftp_upload.pl -c contactListMobile
