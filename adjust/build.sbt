a42ffecbc363123763eb7a9e2f3700a916c4ad19
Harbinder <harbinder@jabong.com>
2015-10-07 17:50:34 +0530

    Added separate directory for adjust feed

---
+++ adjust/build.sbt
@@ -0,0 +1,47 @@
+name := "Alchemy"
+
+version := "0.1.13"
+
+scalaVersion := "2.10.5"
+
+resolvers ++= Seq(
+  Resolver.sonatypeRepo("public")
+)
+
+libraryDependencies += "com.sun.mail" % "javax.mail" % "1.5.3"
+
+libraryDependencies += "javax.mail" % "javax.mail-api" % "1.5.3"
+
+libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "1.2.1"  % "provided"
+
+libraryDependencies += "com.github.scopt" %% "scopt" % "3.3.0"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.1" % "provided"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.4.1" % "provided"
+
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "1.4.1" % "provided"
+
+libraryDependencies += "org.apache.spark" % "spark-launcher_2.10" % "1.4.1" % "provided"
+
+libraryDependencies += "net.liftweb" %% "lift-json" % "2.6"
+
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.35"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4"
+
+libraryDependencies += "com.databricks" % "spark-csv_2.10" % "1.0.3"
+
+libraryDependencies ++= Seq("org.slf4j" % "slf4j-api" % "1.7.5",
+  "org.slf4j" % "slf4j-simple" % "1.7.5",
+  "org.clapper" %% "grizzled-slf4j" % "1.0.1",
+  "org.pegdown" % "pegdown" % "1.0.2")
+
+test in assembly := {}
+assemblyJarName in assembly := "Alchemy-assembly.jar"
+
+(testOptions in Test) += Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/report")
+(testOptions in Test) += Tests.Argument(TestFrameworks.ScalaTest, "-oT")
+
+
+//seq(lsSettings :_*)
---
+++ adjust/src/main/scala/com/jabong/dap/adjust/CsvCreator.scala
@@ -0,0 +1,76 @@
+package main.scala.com.jabong.dap.adjust
+
+import java.net.URLDecoder
+import org.apache.hadoop.io.NullWritable
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
+import org.apache.spark.{SparkConf, HashPartitioner, SparkContext}
+import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat
+
+/**
+ * Created by harbinder on 19/6/15.
+ * Formats raw data present in input directory path cli argument
+ * Creates different file /partition for each event
+ */
+
+class RDDMultipleTextOutputFormat extends MultipleTextOutputFormat[Any, Any] {
+  override def generateActualKey(key: Any, value: Any): Any =
+    NullWritable.get()
+
+  override def generateFileNameForKeyValue(key: Any, value: Any, name: String): String =
+    key.asInstanceOf[String]
+}
+
+object CsvCreator {
+
+  val headerString = Array("event", "?os", "environment", "user_id", "campaign", "adid", "google_play_advertising_id", "store", "reftag",  "adgroup", "creative",
+    "transaction_id", "payment_option_selected", "revenue", "price", "sku", "skus",
+    "ip", "country", "shop_country", "currency_code", "os_version", "network", "network_class",
+    "device", "DeviceID", "device_id", "wp_device_id", "device_model", "device_name", "device_manufacturer", "display_size",
+    "bundle", "app_version", "sdk_version", "user_agent", "session", "language", "tracking_enabled",
+    "time", "install_time", "Date", "time_spent", "timezone", "click_time", "duration", "last_time_spent",
+    "fb_campaign_name", "fb_campaign_id", "fb_adset_name", "fb_adset_id", "fb_ad_name", "fb_ad_id"
+  )
+  val Headers = headerString
+
+  def main(args: Array[String]) {
+    if(args.length < 2){
+      println("Please specify all arguments:- InputDirectory  OutputDirectory")
+      sys.exit(0)
+    }
+
+    val conf = new SparkConf().setAppName("Adjust Log Csv")
+    val sc = new SparkContext(conf)
+
+    //TODO: merge all replace in one
+    val lineArr = sc.textFile(args(0))
+      .map(line => line.replace('&', ',')
+      .replace('=', ',')
+      .replace("/v1/events", ",")
+      .replace("transaction_ID", "transaction_id")
+      .replace("order_value", "price")
+      .split(",")
+      )
+      .map(lineArr => ArrtoSortedRow(lineArr))
+      .filter(row => !row.isEmpty)
+      .map(row => (row.substring(0, row.indexOf("eEND,")), row.substring(row.indexOf("eEND,")+5, row.length)))
+      .partitionBy(new HashPartitioner(50))
+      .saveAsHadoopFile(args(1), classOf[String], classOf[String], classOf[RDDMultipleTextOutputFormat])
+  }
+
+  def ArrtoSortedRow(lineArr:Array[String]): String = {
+
+    var sortedRow = "" //headerString.toString
+    for (col <- Headers) {
+      if(lineArr(lineArr.indexOf(col).toInt + 1).isEmpty) {
+        sortedRow = sortedRow + "null" + ","
+      } else {
+        if (col == "event") {
+          sortedRow = sortedRow + lineArr(lineArr.indexOf(col).toInt + 1).toString + "eEND,"
+        } else {
+          sortedRow = sortedRow + URLDecoder.decode(lineArr(lineArr.indexOf(col).toInt + 1).toString, "UTF-8").replace(",", "|").toString + ","
+        }
+      }
+    }
+    return sortedRow.substring(0, sortedRow.length - 1)
+  }
+}
