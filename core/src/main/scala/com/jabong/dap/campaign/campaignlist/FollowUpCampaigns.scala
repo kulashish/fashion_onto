bf3de57e83d7b3c6d0ff91547d0d85e864de7fb9
Rahul Aneja <rahul.aneja@jabong.com>
2015-10-14 11:36:26 +0530

    added follow up campaign test files and main file

---
+++ core/src/main/scala/com/jabong/dap/campaign/campaignlist/FollowUpCampaigns.scala
@@ -0,0 +1,24 @@
+package com.jabong.dap.campaign.campaignlist
+
+import com.jabong.dap.campaign.data.CampaignOutput
+import com.jabong.dap.campaign.utils.CampaignUtils
+import com.jabong.dap.common.constants.campaign.{CampaignCommon, CampaignMergedFields}
+import com.jabong.dap.data.storage.DataSets
+import org.apache.spark.sql.DataFrame
+
+/**
+ * Created by rahul on 13/10/15.
+ */
+class FollowUpCampaigns {
+
+  def runCampaign(campaignMergedData:DataFrame , salesOrderLast3Days: DataFrame , itrSkuSimpleYesterdayData:DataFrame): Unit ={
+
+    val selectedData = CampaignUtils.campaignSelection(campaignMergedData,salesOrderLast3Days)
+    
+    val refSku1Out = CampaignUtils.campaignSkuStockSelection(selectedData,itrSkuSimpleYesterdayData,CampaignMergedFields.LIVE_REF_SKU1,3)
+    val refSku2Out = CampaignUtils.campaignSkuStockSelection(refSku1Out,itrSkuSimpleYesterdayData,CampaignMergedFields.LIVE_REF_SKU+"2",3)
+    val refSku3Out = CampaignUtils.campaignSkuStockSelection(refSku2Out,itrSkuSimpleYesterdayData,CampaignMergedFields.LIVE_REF_SKU+"3",3)
+
+    CampaignOutput.saveCampaignDataForYesterday(refSku3Out,CampaignCommon.FOLLOW_UP_CAMPAIGNS,DataSets.EMAIL_CAMPAIGNS)
+  }
+}
