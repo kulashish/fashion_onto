5b85f5fb946fe6ec5eba46f65b37ad5412cd422c
Pooja Jain <pooja.jain@jabong.com>
2015-10-15 09:57:06 +0530

    added 3 more files download for the open, bounce and click records from responsys

--- core/scripts/responsys.pl
+++ core/scripts/responsys.pl
@@ -12,14 +12,29 @@ chdir("/data/responsys/");
 
 # getting SMS OPT OUT and DND files
 system("lftp -c 'set sftp:connect-program \"ssh -a -x -i ./u1.pem\"; connect sftp://jabong_scp:dummy\@files.dc2.responsys.net; mget download/53699_SMS_OPT_OUT_$date_with_zero" . "_* ;'");
-system("lftp -c 'set sftp:connect-program \"ssh -a -x -i ./u1.pem\"; connect sftp://jabong_scp:dummy\@files.dc2.responsys.net; mget download/53699_SMS_DELIVERED_$date_with_zero" . "_* ;'");
-
 # copy data to hdfs
 system("hadoop fs -mkdir -p /data/input/responsys/sms_opt_out/daily/$date/");
 system("hadoop fs -copyFromLocal 53699_SMS_OPT_OUT_$date_with_zero" . "_*.txt /data/input/responsys/sms_opt_out/daily/$date/53699_SMS_OPT_OUT_$date_with_zero" . ".txt");
 
+system("lftp -c 'set sftp:connect-program \"ssh -a -x -i ./u1.pem\"; connect sftp://jabong_scp:dummy\@files.dc2.responsys.net; mget download/53699_SMS_DELIVERED_$date_with_zero" . "_* ;'");
+# copy data to hdfs
 system("hadoop fs -mkdir -p /data/input/responsys/sms_delivered/daily/$date/");
 system("hadoop fs -copyFromLocal 53699_SMS_DELIVERED_$date_with_zero" . "_*.txt /data/input/responsys/sms_delivered/daily/$date/53699_SMS_DELIVERED_$date_with_zero" . ".txt");
 
 system("perl /opt/alchemy-core/current/bin/run.pl -t PROD -c dndMerger");
 system("perl /opt/alchemy-core/current/bin/run.pl -t PROD -c smsOptOutMerger");
+
+system("lftp -c 'set sftp:connect-program \"ssh -a -x -i ./u1.pem\"; connect sftp://jabong_scp:dummy\@files.dc2.responsys.net; mget download/53699_OPEN_$date_with_zero" . "_* ;'");
+# copy data to hdfs
+system("hadoop fs -mkdir -p /data/input/responsys/open/daily/$date/");
+system("hadoop fs -copyFromLocal 53699_OPEN_$date_with_zero" . "_*.txt /data/input/responsys/open/daily/$date/53699_OPEN_$date_with_zero" . ".txt");
+
+system("lftp -c 'set sftp:connect-program \"ssh -a -x -i ./u1.pem\"; connect sftp://jabong_scp:dummy\@files.dc2.responsys.net; mget download/53699_BOUNCE_$date_with_zero" . "_* ;'");
+# copy data to hdfs
+system("hadoop fs -mkdir -p /data/input/responsys/bounce/daily/$date/");
+system("hadoop fs -copyFromLocal 53699_BOUNCE_$date_with_zero" . "_*.txt /data/input/responsys/bounce/daily/$date/53699_BOUNCE_$date_with_zero" . ".txt");
+
+system("lftp -c 'set sftp:connect-program \"ssh -a -x -i ./u1.pem\"; connect sftp://jabong_scp:dummy\@files.dc2.responsys.net; mget download/53699_CLICK_$date_with_zero" . "_* ;'");
+# copy data to hdfs
+system("hadoop fs -mkdir -p /data/input/responsys/click/daily/$date/");
+system("hadoop fs -copyFromLocal 53699_CLICK_$date_with_zero" . "_*.txt /data/input/responsys/click/daily/$date/53699_CLICK_$date_with_zero" . ".txt");
