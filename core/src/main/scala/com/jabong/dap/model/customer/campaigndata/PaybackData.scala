2b2a9a08aff4f91065ebd6535cc731eec4f76eed
raghu <raghu@raghu1381>
2015-10-23 14:37:48 +0530

    added PaybackData

--- core/src/main/scala/com/jabong/dap/model/customer/campaigndata/PaybackData.scala
+++ core/src/main/scala/com/jabong/dap/model/customer/campaigndata/PaybackData.scala
@@ -1,13 +1,16 @@
 package com.jabong.dap.model.customer.campaigndata
 
 import com.jabong.dap.common.OptionUtils
+import com.jabong.dap.common.constants.SQL
 import com.jabong.dap.common.constants.config.ConfigConstants
+import com.jabong.dap.common.constants.variables.{ ContactListMobileVars, CustomerVariables, SalesOrderVariables }
 import com.jabong.dap.common.time.{ TimeConstants, TimeUtils }
 import com.jabong.dap.data.acq.common.ParamInfo
 import com.jabong.dap.data.read.DataReader
 import com.jabong.dap.data.storage.DataSets
 import com.jabong.dap.data.write.DataWriter
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
 
 /**
  * Created by raghu on 20/10/15.
@@ -17,14 +20,21 @@ object PaybackData {
   def start(params: ParamInfo) = {
 
     val incrDate = OptionUtils.getOptValue(params.incrDate, TimeUtils.getDateAfterNDays(-1, TimeConstants.DATE_FORMAT_FOLDER))
+    val saveMode = params.saveMode
     val paths = OptionUtils.getOptValue(params.path)
+    val prevDate = OptionUtils.getOptValue(params.fullDate, TimeUtils.getDateAfterNDays(-1, TimeConstants.DATE_FORMAT_FOLDER, incrDate))
 
-    val (salesOrder, paymentPrepaidTransactionData, paymentBankPriority, soPaybackEarn, soPaybackRedeem) = readDF(paths, incrDate)
+    val (salesOrder, paymentPrepaidTransactionData, paymentBankPriority, soPaybackEarn, soPaybackRedeem, dfCmrFull, privFullPayback) = readDF(paths, incrDate, prevDate)
 
-    val (dfPaybackData) = getPaybackData(salesOrder, paymentPrepaidTransactionData, paymentBankPriority, soPaybackEarn, soPaybackRedeem)
+    val (incPaybackData, fullPaybackData) = getPaybackData(salesOrder, paymentPrepaidTransactionData, paymentBankPriority, soPaybackEarn, soPaybackRedeem, dfCmrFull, privFullPayback)
+
+    val pathPaybackDataFull = DataWriter.getWritePath(ConfigConstants.WRITE_OUTPUT_PATH, DataSets.VARIABLES, DataSets.PAYBACK_DATA, DataSets.FULL_MERGE_MODE, incrDate)
+    if (DataWriter.canWrite(saveMode, pathPaybackDataFull)) {
+      DataWriter.writeParquet(fullPaybackData, pathPaybackDataFull, saveMode)
+    }
 
     val fileDate = TimeUtils.changeDateFormat(TimeUtils.getDateAfterNDays(1, TimeConstants.DATE_FORMAT_FOLDER, incrDate), TimeConstants.DATE_FORMAT_FOLDER, TimeConstants.YYYYMMDD)
-    DataWriter.writeCsv(dfPaybackData.na.fill(""), DataSets.VARIABLES, DataSets.PAYBACK_DATA, DataSets.DAILY_MODE, incrDate, "53699_78384_" + fileDate + "_payback_data", DataSets.IGNORE_SAVEMODE, "true", ";")
+    DataWriter.writeCsv(incPaybackData.na.fill(""), DataSets.VARIABLES, DataSets.PAYBACK_DATA, DataSets.DAILY_MODE, incrDate, "53699_78384_" + fileDate + "_payback_data", DataSets.IGNORE_SAVEMODE, "true", ";")
 
   }
 
@@ -37,8 +47,64 @@ object PaybackData {
    * @param soPaybackRedeem
    * @return
    */
-  def getPaybackData(salesOrder: DataFrame, paymentPrepaidTransactionData: DataFrame, paymentBankPriority: DataFrame, soPaybackEarn: DataFrame, soPaybackRedeem: DataFrame): (DataFrame) = {
-    null
+  def getPaybackData(salesOrder: DataFrame, paymentPrepaidTransactionData: DataFrame, paymentBankPriority: DataFrame, soPaybackEarn: DataFrame, soPaybackRedeem: DataFrame, dfCmrFull: DataFrame, privFullPayback: DataFrame): (DataFrame, DataFrame) = {
+
+    val dfIcici = salesOrder.join(paymentPrepaidTransactionData, salesOrder(SalesOrderVariables.ID_SALES_ORDER) === paymentPrepaidTransactionData(SalesOrderVariables.FK_SALES_ORDER), SQL.INNER)
+      .join(paymentBankPriority, paymentPrepaidTransactionData("bank_code") === paymentBankPriority("bank_code"), SQL.INNER)
+      .select(
+        salesOrder(SalesOrderVariables.FK_CUSTOMER) as CustomerVariables.ID_CUSTOMER,
+        lit(1) as "icici_debitcard"
+      )
+
+    val dfEarn = salesOrder.join(soPaybackEarn, salesOrder(SalesOrderVariables.ID_SALES_ORDER) === soPaybackEarn(SalesOrderVariables.FK_SALES_ORDER), SQL.INNER)
+      .select(
+        salesOrder(SalesOrderVariables.FK_CUSTOMER),
+        lit(1) as "earn_points"
+      )
+
+    val dfBurn = salesOrder.join(soPaybackRedeem, salesOrder(SalesOrderVariables.ID_SALES_ORDER) === soPaybackRedeem(SalesOrderVariables.FK_SALES_ORDER), SQL.INNER)
+      .select(
+        salesOrder(SalesOrderVariables.FK_CUSTOMER),
+        lit(1) as "burn_points"
+      )
+
+    val dfIciciEarn = dfIcici.join(dfEarn, dfIcici(CustomerVariables.ID_CUSTOMER) === dfEarn(SalesOrderVariables.FK_CUSTOMER))
+      .select(
+        coalesce(dfIcici(CustomerVariables.ID_CUSTOMER), dfEarn(SalesOrderVariables.FK_CUSTOMER)) as CustomerVariables.ID_CUSTOMER,
+        dfIcici("icici_debitcard"),
+        dfEarn("earn_points")
+      )
+
+    val dfIciciEarnBurn = dfIciciEarn.join(dfBurn, dfIciciEarn(CustomerVariables.ID_CUSTOMER) === dfBurn(SalesOrderVariables.FK_CUSTOMER))
+      .select(
+        coalesce(dfIcici(CustomerVariables.ID_CUSTOMER), dfEarn(SalesOrderVariables.FK_CUSTOMER)) as CustomerVariables.ID_CUSTOMER,
+        dfIcici("icici_debitcard"),
+        dfEarn("earn_points"),
+        dfEarn("burn_points")
+      ).na.fill(0)
+
+    val dfCmr = dfCmrFull.select(
+      dfCmrFull(ContactListMobileVars.UID),
+      dfCmrFull(CustomerVariables.ID_CUSTOMER)
+    )
+
+    val dfInc = dfIciciEarnBurn.join(dfCmr, dfCmr(CustomerVariables.ID_CUSTOMER) === dfIciciEarnBurn(CustomerVariables.ID_CUSTOMER), SQL.INNER)
+      .select(
+        dfCmr(ContactListMobileVars.UID),
+        dfIciciEarnBurn("icici_debitcard"),
+        dfIciciEarnBurn("earn_points"),
+        dfIciciEarnBurn("burn_points"),
+        lit(1) as "payback"
+      ).distinct
+
+    if (privFullPayback != null) {
+
+      val df = dfInc.filter(dfInc(ContactListMobileVars.UID) !== privFullPayback(ContactListMobileVars.UID))
+
+      (df, privFullPayback.unionAll(df))
+    }
+
+    (dfInc, dfInc)
   }
 
   /**
@@ -47,7 +113,7 @@ object PaybackData {
    * @param incrDate
    * @return
    */
-  def readDF(paths: String, incrDate: String): (DataFrame, DataFrame, DataFrame, DataFrame, DataFrame) = {
+  def readDF(paths: String, incrDate: String, prevDate: String): (DataFrame, DataFrame, DataFrame, DataFrame, DataFrame, DataFrame, DataFrame) = {
 
     if (paths != null) {
 
@@ -57,7 +123,10 @@ object PaybackData {
       val dfPaybackEarn = DataReader.getDataFrame(ConfigConstants.INPUT_PATH, DataSets.BOB, DataSets.SALES_ORDER_PAYBACK_EARN, DataSets.FULL_MERGE_MODE, incrDate)
       val dfPaybackRedeem = DataReader.getDataFrame(ConfigConstants.INPUT_PATH, DataSets.BOB, DataSets.SALES_ORDER_PAYBACK_REDEEM, DataSets.FULL_MERGE_MODE, incrDate)
 
-      (dfSalesOrder, dfPaymentPrepaidTransactionData, dfPaymentBankPriority, dfPaybackEarn, dfPaybackRedeem)
+      val dfCmrFull = DataReader.getDataFrame(ConfigConstants.READ_OUTPUT_PATH, DataSets.EXTRAS, DataSets.DEVICE_MAPPING, DataSets.FULL_MERGE_MODE, incrDate)
+      val dfPrivFullPayback = DataReader.getDataFrame(ConfigConstants.WRITE_OUTPUT_PATH, DataSets.VARIABLES, DataSets.PAYBACK_DATA, DataSets.FULL_MERGE_MODE, incrDate)
+
+      (dfSalesOrder, dfPaymentPrepaidTransactionData, dfPaymentBankPriority, dfPaybackEarn, dfPaybackRedeem, dfCmrFull, null)
     } else {
 
       val dfSalesOrder = DataReader.getDataFrame(ConfigConstants.INPUT_PATH, DataSets.BOB, DataSets.SALES_ORDER, DataSets.DAILY_MODE, incrDate)
@@ -66,7 +135,10 @@ object PaybackData {
       val dfPaybackEarn = DataReader.getDataFrame(ConfigConstants.INPUT_PATH, DataSets.BOB, DataSets.SALES_ORDER_PAYBACK_EARN, DataSets.DAILY_MODE, incrDate)
       val dfPaybackRedeem = DataReader.getDataFrame(ConfigConstants.INPUT_PATH, DataSets.BOB, DataSets.SALES_ORDER_PAYBACK_REDEEM, DataSets.DAILY_MODE, incrDate)
 
-      (dfSalesOrder, dfPaymentPrepaidTransactionData, dfPaymentBankPriority, dfPaybackEarn, dfPaybackRedeem)
+      val dfCmrFull = DataReader.getDataFrame(ConfigConstants.READ_OUTPUT_PATH, DataSets.EXTRAS, DataSets.DEVICE_MAPPING, DataSets.FULL_MERGE_MODE, incrDate)
+      val dfPrivFullPayback = DataReader.getDataFrame(ConfigConstants.WRITE_OUTPUT_PATH, DataSets.VARIABLES, DataSets.PAYBACK_DATA, DataSets.FULL_MERGE_MODE, incrDate)
+
+      (dfSalesOrder, dfPaymentPrepaidTransactionData, dfPaymentBankPriority, dfPaybackEarn, dfPaybackRedeem, dfCmrFull, dfPrivFullPayback)
     }
   }
 
