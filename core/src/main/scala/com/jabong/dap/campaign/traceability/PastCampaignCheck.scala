48bf2dd4d08b9aff32d2e88315c8a362aa0c4b01
Rahul Aneja <rahul.aneja@jabong.com>
2015-10-26 14:51:44 +0530

    removed comments

--- core/src/main/scala/com/jabong/dap/campaign/traceability/PastCampaignCheck.scala
+++ core/src/main/scala/com/jabong/dap/campaign/traceability/PastCampaignCheck.scala
@@ -142,19 +142,6 @@ object PastCampaignCheck extends Logging {
 
     val pastCampaignSendCustomers = getCampaignCustomers(pastCampaignData, campaignMailType, nDays, DataSets.EMAIL_CAMPAIGNS)
 
-//    var dfCustomerSkuSimpleSelected = customerSkuSimpleSelected
-
-//    if (!customerSkuSimpleSelected.schema.fieldNames.toList.contains(CustomerVariables.EMAIL)) {
-//      dfCustomerSkuSimpleSelected = customerSkuSimpleSelected.withColumn(CustomerVariables.EMAIL, lit(null))
-//    }
-//
-//    val customerSkuSelected = dfCustomerSkuSimpleSelected.
-//      withColumn("temp_" + ProductVariables.SKU, Udf.skuFromSimpleSku(dfCustomerSkuSimpleSelected(ProductVariables.SKU_SIMPLE)))
-//
-//    val customerSkuSelectedEmail = customerSkuSelected.filter(CustomerVariables.FK_CUSTOMER + " is null or " + CustomerVariables.FK_CUSTOMER + " < 1")
-//
-//    val customerSkuSelectedId = customerSkuSelected.filter(CustomerVariables.FK_CUSTOMER + " is not null or " + CustomerVariables.FK_CUSTOMER + " >= 1")
-
     val customerSkuSelected = customerSkuSimpleSelected.
       withColumn(ProductVariables.SKU, Udf.skuFromSimpleSku(customerSkuSimpleSelected(ProductVariables.SKU_SIMPLE)))
 
@@ -173,25 +160,7 @@ object PastCampaignCheck extends Logging {
       .select(
         customerSkuSimpleSelected("*")
       )
-
-//    val pastCampaignNotSendCustomersId = customerSkuSelectedId
-//      .join(pastCampaignSendCustomers,
-//        (customerSkuSelectedId(CustomerVariables.FK_CUSTOMER) === pastCampaignSendCustomers(CampaignMergedFields.CUSTOMER_ID))
-//          &&
-//          ((customerSkuSelectedId("temp_" + ProductVariables.SKU) === pastCampaignSendCustomers(CampaignMergedFields.LIVE_REF_SKU1))
-//            ||
-//            (customerSkuSelectedId("temp_" + ProductVariables.SKU) === pastCampaignSendCustomers(CampaignMergedFields.LIVE_REF_SKU + "2"))
-//            ||
-//            (customerSkuSelectedId("temp_" + ProductVariables.SKU) === pastCampaignSendCustomers(CampaignMergedFields.LIVE_REF_SKU + "3"))), SQL.LEFT_OUTER)
-//      .filter(
-//        CampaignMergedFields.CUSTOMER_ID + " is null "
-//      )
-//      .select(
-//        customerSkuSelectedId("*")
-//      )
-
-//    val pastCampaignNotSendCustomers = pastCampaignNotSendEmail.unionAll(pastCampaignNotSendCustomersId)
-
+    
     return pastCampaignNotSendEmail
   }
 
