d1da4da27146d180b1d8f458ff1271581b5aa44b
Samatha Shetty <samatha.shetty@jabong.com>
2015-10-29 11:55:48 +0530

    Check in missed file

---
+++ core/src/main/scala/com/jabong/dap/model/custorder/ParamJsonValidator.scala
@@ -0,0 +1,33 @@
+package com.jabong.dap.model.custorder
+
+import com.jabong.dap.data.acq.common.{ ParamInfo, ParamJobInfo }
+import com.jabong.dap.data.storage.DataSets
+
+/**
+ * Created by pooja on 9/7/15.
+ */
+object ParamJsonValidator {
+  def validateRequiredValues(paramInfo: ParamInfo) = {
+    require(paramInfo.source != null && paramInfo.source.length() != 0, "Source cannot be null or empty.")
+    require(paramInfo.saveMode != null && paramInfo.saveMode.length() != 0, "Save mode cannot be null or empty.")
+    require(paramInfo.saveFormat != null && paramInfo.saveFormat.length() != 0, "Save format cannot be null or empty.")
+  }
+
+  private def validatePossibleValues(paramInfo: ParamInfo) = {
+    val possibleSaveFormats = Array(DataSets.ORC, DataSets.PARQUET)
+    val possibleSaveModes = Array(DataSets.OVERWRITE_SAVEMODE, DataSets.APPEND_SAVEMODE, DataSets.IGNORE_SAVEMODE, DataSets.ERROR_SAVEMODE)
+
+    require(possibleSaveFormats.contains(paramInfo.saveFormat), "Save format '%s' not recognized. Possible values: %s".
+      format(paramInfo.saveFormat, possibleSaveFormats.mkString(",")))
+    require(possibleSaveModes.contains(paramInfo.saveMode), "Save mode '%s' not recognized. Possible values: %s".
+      format(paramInfo.saveMode, possibleSaveModes.mkString(",")))
+  }
+
+  def validate(info: ParamJobInfo) = {
+    for (paramInfo <- info.params) {
+      validateRequiredValues(paramInfo)
+      validatePossibleValues(paramInfo)
+    }
+  }
+
+}
