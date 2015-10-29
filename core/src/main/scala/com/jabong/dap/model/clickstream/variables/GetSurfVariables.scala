5cce8063ace1a1bb557cee902d03558268747c07
Pooja Jain <pooja.jain@jabong.com>
2015-10-29 15:33:15 +0530

    changed ArrayBuffer

--- core/src/main/scala/com/jabong/dap/model/clickstream/variables/GetSurfVariables.scala
+++ core/src/main/scala/com/jabong/dap/model/clickstream/variables/GetSurfVariables.scala
@@ -4,8 +4,8 @@ import java.io.File
 
 import com.jabong.dap.common.constants.config.ConfigConstants
 import com.jabong.dap.common.constants.variables.PageVisitVariables
-import com.jabong.dap.common.time.{ TimeConstants, TimeUtils }
-import com.jabong.dap.common.{ OptionUtils, Spark }
+import com.jabong.dap.common.time.{TimeConstants, TimeUtils}
+import com.jabong.dap.common.{OptionUtils, Spark}
 import com.jabong.dap.data.acq.common.ParamInfo
 import com.jabong.dap.data.read.PathBuilder
 import com.jabong.dap.data.storage.DataSets
@@ -17,10 +17,9 @@ import grizzled.slf4j.Logging
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.hive.HiveContext
-import org.apache.spark.sql.{ DataFrame, Row }
+import org.apache.spark.sql.{DataFrame, Row}
 
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.WrappedArray
 
 /**
  * Created by Divya on 15/7/15.
@@ -56,7 +55,7 @@ object GetSurfVariables extends java.io.Serializable with Logging {
     var today = "_daily"
     var explodedMergedData: DataFrame = null
     if (mergedData != null) {
-      explodedMergedData = mergedData.explode(PageVisitVariables.SKU_LIST, PageVisitVariables.SKU) { str: mutable.WrappedArray[String] => str.toList }
+      explodedMergedData = mergedData.explode(PageVisitVariables.SKU_LIST, PageVisitVariables.SKU) { str: WrappedArray[String] => str.toList }
       //explodedMergedData = mergedData.explode(PageVisitVariables.SKU_LIST, PageVisitVariables.SKU) { str: List[String] => str.toList }
 
       var joinResult = incremental.join(explodedMergedData, incremental(PageVisitVariables.USER_ID + today) === explodedMergedData(PageVisitVariables.USER_ID))
@@ -98,7 +97,7 @@ object GetSurfVariables extends java.io.Serializable with Logging {
     incremental.select("userid_daily", "sku_daily").registerTempTable("tempincremental")
 
     var IncrementalMerge = hiveContext.sql("select userid_daily as userid,collect_list(sku_daily) as skuList from tempincremental group by userid_daily")
-      .map(x => (x(0).toString, date.toString, x(1).asInstanceOf[ArrayBuffer[String]])).toDF(PageVisitVariables.USER_ID, "dt", PageVisitVariables.SKU_LIST)
+      .map(x => (x(0).toString, date.toString, x(1).asInstanceOf[WrappedArray[String]])).toDF(PageVisitVariables.USER_ID, "dt", PageVisitVariables.SKU_LIST)
 
     if (mergedData != null) {
       val yesterMerge = mergedData.filter("dt != '" + filterDate.toString + "'")
