10ae286f49801e645ae01652193b10b54f4956db
Rahul Aneja <rahul.aneja@jabong.com>
2015-11-03 14:13:23 +0530

    formatting the code

--- core/src/main/scala/com/jabong/dap/campaign/utils/CampaignUtils.scala
+++ core/src/main/scala/com/jabong/dap/campaign/utils/CampaignUtils.scala
@@ -145,7 +145,7 @@ object CampaignUtils extends Logging {
 
     val registeredCustomerRefSku = customerFilteredData.filter(CustomerVariables.FK_CUSTOMER + " != 0  and " + CustomerVariables.FK_CUSTOMER + " is not null")
 
-    val registeredRefSkus = GroupedUtils.orderGroupBy(deviceOnlyCustomerRefSku, groupedFields1, aggFields, GroupedUtils.FIRST, OrderBySchema.pushSurfReferenceSku, ProductVariables.SPECIAL_PRICE, GroupedUtils.DESC, DecimalType.apply())
+    val registeredRefSkus = GroupedUtils.orderGroupBy(registeredCustomerRefSku, groupedFields1, aggFields, GroupedUtils.FIRST, OrderBySchema.pushSurfReferenceSku, ProductVariables.SPECIAL_PRICE, GroupedUtils.DESC, DecimalType.apply())
 
     val customerRefSku = deviceOnlyRefSkus.unionAll(registeredRefSkus)
 
