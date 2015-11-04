8d59ca6c4592bf0ed38dee6bdc3464c8d2698825
Rahul Aneja <rahul.aneja@jabong.com>
2015-11-04 12:51:36 +0530

    added acart Hourly campaign

---
+++ core/src/main/scala/com/jabong/dap/campaign/campaignlist/AcartDailyCampaign.scala
@@ -0,0 +1,37 @@
+package com.jabong.dap.campaign.campaignlist
+
+import com.jabong.dap.campaign.data.CampaignOutput
+import com.jabong.dap.campaign.manager.CampaignProducer
+import com.jabong.dap.campaign.skuselection.Daily
+import com.jabong.dap.campaign.utils.CampaignUtils
+import com.jabong.dap.common.constants.campaign.{ CustomerSelection, CampaignCommon }
+import com.jabong.dap.data.storage.DataSets
+import org.apache.spark.sql.DataFrame
+
+/**
+ * Created by rahul for com.jabong.dap.campaign.campaignlist on 23/7/15.
+ */
+class AcartDailyCampaign {
+
+  def runCampaign(yesterdayAcartData: DataFrame, yesterdaySalesOrderData: DataFrame, yesterdaySalesOrderItemData: DataFrame, yesterdayItrData: DataFrame, brickMvpRecommendations: DataFrame): Unit = {
+
+    val acartCustomerSelector = CampaignProducer.getFactory(CampaignCommon.CUSTOMER_SELECTOR)
+      .getCustomerSelector(CustomerSelection.ACART)
+    //FIXME:Filter the order items data for last 1 day
+    val selectedCustomers = acartCustomerSelector.customerSelection(yesterdayAcartData, yesterdaySalesOrderData, yesterdaySalesOrderItemData)
+
+    CampaignUtils.debug(selectedCustomers, "AcartDaily:-after customer selection")
+    //sku selection
+    //filter sku based on daily filter
+    val filteredSku = Daily.skuFilter(selectedCustomers, yesterdayItrData).cache()
+
+    CampaignUtils.debug(filteredSku, "AcartDaily:-after filteredSku")
+
+    // ***** mobile push use case
+    CampaignUtils.campaignPostProcess(DataSets.PUSH_CAMPAIGNS, CampaignCommon.ACART_DAILY_CAMPAIGN, filteredSku, false)
+
+    // ***** email use case
+    CampaignUtils.campaignPostProcess(DataSets.EMAIL_CAMPAIGNS, CampaignCommon.ACART_DAILY_CAMPAIGN, filteredSku, false, brickMvpRecommendations)
+  }
+}
+
