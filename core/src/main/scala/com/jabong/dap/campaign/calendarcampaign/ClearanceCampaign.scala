1674b1080b206a55b1752361636f015321a7e665
Rahul Aneja <rahul.aneja@jabong.com>
2015-11-13 11:43:59 +0530

    reformat the code

--- core/src/main/scala/com/jabong/dap/campaign/calendarcampaign/ClearanceCampaign.scala
+++ core/src/main/scala/com/jabong/dap/campaign/calendarcampaign/ClearanceCampaign.scala
@@ -4,9 +4,9 @@ import com.jabong.dap.campaign.manager.CampaignProducer
 import com.jabong.dap.campaign.skuselection.Daily
 import com.jabong.dap.campaign.utils.CampaignUtils
 import com.jabong.dap.common.Utils
-import com.jabong.dap.common.constants.campaign.{CustomerSelection, CampaignCommon}
-import com.jabong.dap.common.constants.variables.{SalesOrderItemVariables, SalesOrderVariables}
-import com.jabong.dap.common.time.{TimeConstants, TimeUtils}
+import com.jabong.dap.common.constants.campaign.{ CustomerSelection, CampaignCommon }
+import com.jabong.dap.common.constants.variables.{ SalesOrderItemVariables, SalesOrderVariables }
+import com.jabong.dap.common.time.{ TimeConstants, TimeUtils }
 import com.jabong.dap.data.storage.DataSets
 import com.jabong.dap.model.order.variables.SalesOrder
 import org.apache.spark.sql.DataFrame
@@ -16,12 +16,12 @@ import org.apache.spark.sql.DataFrame
  */
 class ClearanceCampaign {
 
-  def runCampaign(last30DaysSalesOrderData: DataFrame, last30DaysSalesOrderItemData: DataFrame, mvpDiscountRecommendations: DataFrame, yesterdayItrData: DataFrame, incrDate: String  = TimeUtils.getDateAfterNDays(-1,TimeConstants.DATE_TIME_FORMAT)) = {
+  def runCampaign(last30DaysSalesOrderData: DataFrame, last30DaysSalesOrderItemData: DataFrame, mvpDiscountRecommendations: DataFrame, yesterdayItrData: DataFrame, incrDate: String = TimeUtils.getDateAfterNDays(-1, TimeConstants.DATE_TIME_FORMAT)) = {
 
-   val last10thSalesOrderData =  Utils.getOneDayData(last30DaysSalesOrderData,SalesOrderVariables.CREATED_AT,TimeUtils.getDateAfterNDays(-10,TimeConstants.DATE_TIME_FORMAT,incrDate),TimeConstants.DATE_TIME_FORMAT)
-    val last10thSalesOrderItemData =  Utils.getOneDayData(last30DaysSalesOrderItemData,SalesOrderItemVariables.CREATED_AT,TimeUtils.getDateAfterNDays(-10,TimeConstants.DATE_TIME_FORMAT,incrDate),TimeConstants.DATE_TIME_FORMAT)
-    val last30thSalesOrderData =  Utils.getOneDayData(last30DaysSalesOrderData,SalesOrderVariables.CREATED_AT,TimeUtils.getDateAfterNDays(-30,TimeConstants.DATE_TIME_FORMAT,incrDate),TimeConstants.DATE_TIME_FORMAT)
-    val last30thSalesOrderItemData =  Utils.getOneDayData(last30DaysSalesOrderItemData,SalesOrderItemVariables.CREATED_AT,TimeUtils.getDateAfterNDays(-30,TimeConstants.DATE_TIME_FORMAT,incrDate),TimeConstants.DATE_TIME_FORMAT)
+    val last10thSalesOrderData = Utils.getOneDayData(last30DaysSalesOrderData, SalesOrderVariables.CREATED_AT, TimeUtils.getDateAfterNDays(-10, TimeConstants.DATE_TIME_FORMAT, incrDate), TimeConstants.DATE_TIME_FORMAT)
+    val last10thSalesOrderItemData = Utils.getOneDayData(last30DaysSalesOrderItemData, SalesOrderItemVariables.CREATED_AT, TimeUtils.getDateAfterNDays(-10, TimeConstants.DATE_TIME_FORMAT, incrDate), TimeConstants.DATE_TIME_FORMAT)
+    val last30thSalesOrderData = Utils.getOneDayData(last30DaysSalesOrderData, SalesOrderVariables.CREATED_AT, TimeUtils.getDateAfterNDays(-30, TimeConstants.DATE_TIME_FORMAT, incrDate), TimeConstants.DATE_TIME_FORMAT)
+    val last30thSalesOrderItemData = Utils.getOneDayData(last30DaysSalesOrderItemData, SalesOrderItemVariables.CREATED_AT, TimeUtils.getDateAfterNDays(-30, TimeConstants.DATE_TIME_FORMAT, incrDate), TimeConstants.DATE_TIME_FORMAT)
 
     val salesOrderData = last10thSalesOrderData.unionAll(last30thSalesOrderData)
     val salesOrderItemData = last10thSalesOrderItemData.unionAll(last30thSalesOrderItemData)
@@ -36,4 +36,4 @@ class ClearanceCampaign {
     CampaignUtils.campaignPostProcess(DataSets.CALENDAR_CAMPAIGNS, CampaignCommon.CLEARANCE_CAMPAIGN, filteredSku, false, mvpDiscountRecommendations)
 
   }
-  }
+}
--- core/src/test/scala/com/jabong/dap/campaign/calendarcampaign/ClearanceCampaignTest.scala
+++ core/src/test/scala/com/jabong/dap/campaign/calendarcampaign/ClearanceCampaignTest.scala
@@ -4,11 +4,11 @@ import com.jabong.dap.campaign.data.CampaignOutput
 import com.jabong.dap.common.constants.campaign.CampaignCommon
 import com.jabong.dap.common.json.JsonUtils
 import com.jabong.dap.common.time.TimeUtils
-import com.jabong.dap.common.{TestSchema, Spark, SharedSparkContext}
+import com.jabong.dap.common.{ TestSchema, Spark, SharedSparkContext }
 import com.jabong.dap.data.storage.DataSets
 import com.jabong.dap.data.storage.schema.Schema
-import org.apache.spark.sql.{DataFrame, SQLContext}
-import org.scalatest.{GivenWhenThen, FeatureSpec}
+import org.apache.spark.sql.{ DataFrame, SQLContext }
+import org.scalatest.{ GivenWhenThen, FeatureSpec }
 
 /**
  * Created by rahul on 10/11/15.
@@ -30,12 +30,11 @@ class ClearanceCampaignTest extends FeatureSpec with GivenWhenThen with SharedSp
     mvpDiscountRecommendations = JsonUtils.readFromJson(DataSets.CAMPAIGNS + "/calendarcampaign", "mvp_discount_recommendation")
   }
 
-
   feature("run Clearance Campaign"){
     scenario("select sku from 20th day last order data"){
       Given("last30DaysSalesOrderData, last30DaysSalesOrderItemData, mvpDiscountRecommendations, yesterdayItrSkuSimpleData")
       val clearanceCampaign = new ClearanceCampaign()
-      clearanceCampaign.runCampaign(last30DaysSalesOrderData, last30DaysSalesOrderItemData, mvpDiscountRecommendations, yesterdayItrData,"2015-11-13 23:43:43.0")
+      clearanceCampaign.runCampaign(last30DaysSalesOrderData, last30DaysSalesOrderItemData, mvpDiscountRecommendations, yesterdayItrData, "2015-11-13 23:43:43.0")
 
       val pricepointCampaignOut = CampaignOutput.testData.head
       //      assert(pricepointCampaignOut._1.count() == 1)
