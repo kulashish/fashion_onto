3f82fa16694eb55f1abe0a0f78c719b959fd7fd5
Samatha Shetty <samatha.shetty@jabong.com>
2015-11-16 12:53:57 +0530

    Manual Merge   changes

--- core/src/main/scala/com/jabong/dap/campaign/recommendation/LiveCommonRecommender.scala
+++ core/src/main/scala/com/jabong/dap/campaign/recommendation/LiveCommonRecommender.scala
@@ -1,5 +1,6 @@
 package com.jabong.dap.campaign.recommendation
 
+import com.jabong.dap.campaign.recommendation.generator.RecommendationUtils
 import com.jabong.dap.campaign.utils.CampaignUtils
 import com.jabong.dap.common.Spark
 import com.jabong.dap.common.constants.campaign.{ Recommendation, CampaignMergedFields }
@@ -27,23 +28,27 @@ class LiveCommonRecommender extends Recommender with Logging {
   override def generateRecommendation(refSkus: DataFrame, recommendations: DataFrame, recType: String = Recommendation.BRICK_MVP_SUB_TYPE, numRecSkus: Int): DataFrame = {
     require(refSkus != null, "refSkus cannot be null")
     require(recommendations != null, "recommendations cannot be null")
-    require(Array(Recommendation.BRICK_MVP_SUB_TYPE, Recommendation.BRAND_MVP_SUB_TYPE) contains recType, "recommendation type is invalid")
+    require(Array(Recommendation.BRICK_MVP_SUB_TYPE, Recommendation.BRAND_MVP_SUB_TYPE, Recommendation.BRICK_PRICE_BAND_SUB_TYPE) contains recType, "recommendation type is invalid")
     var refSkusUpdatedSchema: DataFrame = refSkus
     if (!SchemaUtils.isSchemaEqual(refSkus.schema, Schema.expectedFinalReferenceSku)) {
-      refSkusUpdatedSchema = SchemaUtils.changeSchema(refSkus, Schema.expectedFinalReferenceSku)
+      refSkusUpdatedSchema = SchemaUtils.addColumns(refSkus, Schema.expectedFinalReferenceSku)
     }
 
+    CampaignUtils.debug(recommendations, "after recommendations")
+
     val refSkuExploded = refSkusUpdatedSchema.select(
-      refSkusUpdatedSchema(CustomerVariables.FK_CUSTOMER),
+      refSkusUpdatedSchema(CustomerVariables.EMAIL),
       refSkusUpdatedSchema(CampaignMergedFields.REF_SKU1),
       refSkusUpdatedSchema(CampaignMergedFields.CAMPAIGN_MAIL_TYPE),
       refSkusUpdatedSchema(CampaignMergedFields.LIVE_CART_URL),
       explode(refSkus(CampaignMergedFields.REF_SKUS)) as "ref_sku_fields")
 
+    CampaignUtils.debug(refSkuExploded, "after refSkuExploded")
+
     //FIXME: To check if there is any ref sku in recommended sku
     //FIXME: add column as rec skus instead of passing entire data to genRecSkus function
     val completeRefSku = refSkuExploded.select(
-      refSkuExploded(CustomerVariables.FK_CUSTOMER),
+      refSkuExploded(CustomerVariables.EMAIL),
       refSkuExploded(CampaignMergedFields.REF_SKU1),
       refSkuExploded(CampaignMergedFields.CAMPAIGN_MAIL_TYPE),
       refSkuExploded(CampaignMergedFields.LIVE_CART_URL),
@@ -52,22 +57,17 @@ class LiveCommonRecommender extends Recommender with Logging {
       refSkuExploded("ref_sku_fields.gender") as ProductVariables.GENDER,
       refSkuExploded("ref_sku_fields.brand") as ProductVariables.BRAND,
       refSkuExploded("ref_sku_fields.productName") as ProductVariables.PRODUCT_NAME,
+      refSkuExploded("ref_sku_fields.priceBand") as ProductVariables.PRICE_BAND,
       refSkuExploded("ref_sku_fields.skuSimple") as CampaignMergedFields.REF_SKU)
 
-    var recommendationJoined: DataFrame = null
+    CampaignUtils.debug(completeRefSku, "after completeRefSku")
 
-    if (recType.equalsIgnoreCase(Recommendation.BRICK_MVP_SUB_TYPE)) {
-      recommendationJoined = completeRefSku.join(recommendations, completeRefSku(ProductVariables.BRICK) === recommendations(ProductVariables.BRICK)
-        && completeRefSku(ProductVariables.MVP) === recommendations(ProductVariables.MVP)
-        && completeRefSku(ProductVariables.GENDER) === recommendations(ProductVariables.GENDER))
-    } else {
-      recommendationJoined = completeRefSku.join(recommendations, completeRefSku(ProductVariables.BRAND) === recommendations(ProductVariables.BRAND)
-        && completeRefSku(ProductVariables.MVP) === recommendations(ProductVariables.MVP)
-        && completeRefSku(ProductVariables.GENDER) === recommendations(ProductVariables.GENDER))
-    }
+    val recommendationJoined = joinToRecommendation(completeRefSku, recommendations, recType)
+
+    CampaignUtils.debug(recommendationJoined, "after recommendationJoined")
 
     val recommendationSelected = recommendationJoined.select(
-      completeRefSku(CustomerVariables.FK_CUSTOMER),
+      completeRefSku(CustomerVariables.EMAIL),
       // recommendedSkus(completeRefSku(CampaignMergedFields.REF_SKU), recommendations(CampaignMergedFields.RECOMMENDATIONS)) as CampaignMergedFields.REC_SKUS,
       recommendations(CampaignMergedFields.RECOMMENDATIONS + "." + ProductVariables.SKU) as CampaignMergedFields.REC_SKUS,
       Udf.skuFromSimpleSku(completeRefSku(CampaignMergedFields.REF_SKU)) as CampaignMergedFields.REF_SKU,
@@ -77,15 +77,66 @@ class LiveCommonRecommender extends Recommender with Logging {
       completeRefSku(CampaignMergedFields.CAMPAIGN_MAIL_TYPE),
       completeRefSku(CampaignMergedFields.LIVE_CART_URL))
 
-    val recommendationGrouped = recommendationSelected.map(row => ((row(0)), (row))).groupByKey().map({ case (key, value) => (key.asInstanceOf[Long], getRecSkus(value, numRecSkus)) })
+    CampaignUtils.debug(recommendationSelected, "after recommendationSelected")
+
+    val recommendationGrouped = recommendationSelected.map(row => ((row(0)), (row))).groupByKey().map({ case (key, value) => (key.asInstanceOf[String], getRecSkus(value,  numRecSkus)) })
       .map({ case (key, value) => (key, value._1, value._2, value._3, value._4) })
 
     val sqlContext = Spark.getSqlContext()
     import sqlContext.implicits._
-    val campaignDataWithRecommendations = recommendationGrouped.toDF(CustomerVariables.FK_CUSTOMER, CampaignMergedFields.REF_SKUS,
+    val campaignDataWithRecommendations = recommendationGrouped.toDF(CustomerVariables.EMAIL, CampaignMergedFields.REF_SKUS,
       CampaignMergedFields.REC_SKUS, CampaignMergedFields.CAMPAIGN_MAIL_TYPE, CampaignMergedFields.LIVE_CART_URL)
 
+    CampaignUtils.debug(campaignDataWithRecommendations, "after campaignDataWithRecommendations")
+
     return campaignDataWithRecommendations
+  }
+
+  def joinToRecommendation(completeRefSku: DataFrame, recommendations: DataFrame, recType: String): DataFrame = {
+
+    require(completeRefSku != null, "completeRefSku data frame should not null")
+    require(recommendations != null, "recommendations data frame should not null")
+    require(RecommendationUtils.recommendationType.contains(recType), "recommendations data frame should not null")
+
+    val recommendationJoined = recType match {
+      case Recommendation.BRAND_MVP_SUB_TYPE => {
+        completeRefSku.join(recommendations, completeRefSku(ProductVariables.BRAND) === recommendations(ProductVariables.BRAND)
+          && completeRefSku(ProductVariables.MVP) === recommendations(ProductVariables.MVP)
+          && completeRefSku(ProductVariables.GENDER) === recommendations(ProductVariables.GENDER))
+
+      }
+      case Recommendation.BRICK_PRICE_BAND_SUB_TYPE => {
+
+        val dfNextPriceBand = completeRefSku.select(
+          col(CustomerVariables.EMAIL),
+          col(CampaignMergedFields.REF_SKU1),
+          col(CampaignMergedFields.CAMPAIGN_MAIL_TYPE),
+          col(CampaignMergedFields.LIVE_CART_URL),
+          col(ProductVariables.BRICK),
+          col(ProductVariables.MVP),
+          col(ProductVariables.GENDER),
+          col(ProductVariables.BRAND),
+          col(ProductVariables.PRODUCT_NAME),
+          Udf.nextPriceBand(col(ProductVariables.PRICE_BAND)) as ProductVariables.PRICE_BAND,
+          col(CampaignMergedFields.REF_SKU)
+        )
+
+        CampaignUtils.debug(dfNextPriceBand, "inside join dfNextPriceBand")
+        CampaignUtils.debug(completeRefSku, "inside join completeRefSku")
+        CampaignUtils.debug(recommendations, "inside join recommendations")
+
+        completeRefSku.join(recommendations, completeRefSku(ProductVariables.BRICK) === recommendations(ProductVariables.BRICK)
+          && Udf.nextPriceBand(completeRefSku(ProductVariables.PRICE_BAND)) === recommendations(ProductVariables.PRICE_BAND)
+          && completeRefSku(ProductVariables.GENDER) === recommendations(ProductVariables.GENDER))
+      }
+      case _ => {
+        completeRefSku.join(recommendations, completeRefSku(ProductVariables.BRICK) === recommendations(ProductVariables.BRICK)
+          && completeRefSku(ProductVariables.MVP) === recommendations(ProductVariables.MVP)
+          && completeRefSku(ProductVariables.GENDER) === recommendations(ProductVariables.GENDER))
+      }
+    }
+
+    return recommendationJoined
   }
 
   //  val recommendedSkus = udf((refSkus: String, recommendations: List[((Row))]) => getRecommendedSkus(refSkus: String, recommendations: List[(Row)]))
