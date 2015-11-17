6fb6c065e449aead692db8c04a15b5a6895ae06a
Rahul Aneja <rahul.aneja@jabong.com>
2015-11-17 12:53:14 +0530

    fixed run.pl

--- core/scripts/run.pl
+++ core/scripts/run.pl
@@ -302,7 +302,7 @@ if ($component eq "bobAcqFull1") {
     $AMMUNITION = "--num-executors 10 --executor-memory 4G";
     my $command = "$BASE_SPARK_SUBMIT $AMMUNITION $HIVE_JARS $CORE_JAR --component customerAppDetails --config $HDFS_CONF/config.json --paramJson $HDFS_CONF/customerAppDetails.json";
     $job_exit = run_component($component, $command);
-}} elsif ($component eq "cityWiseData") {
+} elsif ($component eq "cityWiseData") {
      my $command = "$BASE_SPARK_SUBMIT $AMMUNITION  $CORE_JAR --component city_wise_data --config $HDFS_CONF/config.json --paramJson $HDFS_CONF/cityWiseData.json";
      $job_exit = run_component($component, $command);
  } else {
