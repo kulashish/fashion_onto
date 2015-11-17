cd59a700039986e5abedfa04c7ed1a088d070659
Pooja Jain <pooja.jain@jabong.com>
2015-11-17 11:15:17 +0530

    changes for sales_order_Address fav data

--- core/src/main/scala/com/jabong/dap/model/order/variables/SalesOrderAddress.scala
+++ core/src/main/scala/com/jabong/dap/model/order/variables/SalesOrderAddress.scala
@@ -5,18 +5,17 @@ import java.sql.Timestamp
 import com.jabong.dap.common.constants.SQL
 import com.jabong.dap.common.constants.config.ConfigConstants
 import com.jabong.dap.common.constants.variables._
-import com.jabong.dap.common.time.{TimeConstants, TimeUtils}
-import com.jabong.dap.common.{Spark, Utils}
+import com.jabong.dap.common.time.{ TimeConstants, TimeUtils }
+import com.jabong.dap.common.{ Spark, Utils }
 import com.jabong.dap.data.read.DataReader
 import com.jabong.dap.data.storage.DataSets
 import com.jabong.dap.data.storage.schema.Schema
 import com.jabong.dap.data.write.DataWriter
 import com.jabong.dap.model.dataFeeds.DataFeedsModel
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.{ DataFrame, Row }
 
-import scala.collection.mutable.{HashMap, ListBuffer, Map}
-
+import scala.collection.mutable.{ HashMap, ListBuffer, Map }
 
 object SalesOrderAddress extends DataFeedsModel {
 
