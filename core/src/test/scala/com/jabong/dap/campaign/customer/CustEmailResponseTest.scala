24a9533ac574613d2ca8e273d865321705a617cc
Samatha Shetty <samatha.shetty@jabong.com>
2015-11-23 17:02:03 +0530

    Use UID instead of customer_id in csv files, ensuring any schema changes

--- core/src/test/scala/com/jabong/dap/campaign/customer/CustEmailResponseTest.scala
+++ core/src/test/scala/com/jabong/dap/campaign/customer/CustEmailResponseTest.scala
@@ -5,6 +5,7 @@ import com.jabong.dap.common.constants.config.ConfigConstants
 import com.jabong.dap.common.constants.variables.{CustomerVariables, ContactListMobileVars, NewsletterVariables, EmailResponseVariables}
 import com.jabong.dap.common.json.JsonUtils
 import com.jabong.dap.common.schema.SchemaUtils
+import com.jabong.dap.common.time.{TimeConstants, TimeUtils}
 import com.jabong.dap.common.{ SharedSparkContext, Spark }
 import com.jabong.dap.data.acq.common.ParamInfo
 import com.jabong.dap.data.read.DataReader
@@ -15,6 +16,7 @@ import com.jabong.dap.model.customer.campaigndata.CustEmailResponse._
 import com.jabong.dap.model.customer.schema.CustEmailSchema
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.IntegerType
 import org.scalatest.FlatSpec
 
 /**
@@ -49,14 +51,18 @@ class CustEmailResponseTest extends FlatSpec with SharedSparkContext {
     // click counts
     assert(selOpenDf.get(2).toString.equalsIgnoreCase("2"))
 
-    val joinedDf = MergeUtils.joinOldAndNewDF(aggClickData, CustEmailSchema.effectiveSchema,
-      aggOpenData, CustEmailSchema.effectiveSchema, EmailResponseVariables.CUSTOMER_ID)
-      .select(coalesce(aggClickData(EmailResponseVariables.CUSTOMER_ID), aggOpenData(EmailResponseVariables.CUSTOMER_ID)) as EmailResponseVariables.CUSTOMER_ID,
-        aggOpenData(EmailResponseVariables.LAST_OPEN_DATE) as EmailResponseVariables.LAST_OPEN_DATE,
-        aggOpenData(EmailResponseVariables.OPENS_TODAY) as EmailResponseVariables.OPENS_TODAY,
-        aggClickData(EmailResponseVariables.CLICKS_TODAY) as EmailResponseVariables.CLICKS_TODAY,
-        aggClickData(EmailResponseVariables.LAST_CLICK_DATE) as EmailResponseVariables.LAST_CLICK_DATE)
+    val outputCsvFormat = udf((s: String) => TimeUtils.changeDateFormat(s: String, TimeConstants.DD_MMM_YYYY_HH_MM_SS, TimeConstants.DATE_TIME_FORMAT))
 
+    val joinedDf = MergeUtils.joinOldAndNewDF(aggClickData, CustEmailSchema.effectiveSchema,
+      aggOpenData, CustEmailSchema.effectiveSchema, EmailResponseVariables.CUSTOMER_ID, EmailResponseVariables.CUSTOMER_ID)
+      .select(coalesce(col(EmailResponseVariables.CUSTOMER_ID), col(MergeUtils.NEW_ + EmailResponseVariables.CUSTOMER_ID)) as ContactListMobileVars.UID,
+        outputCsvFormat(col(EmailResponseVariables.LAST_OPEN_DATE)) as EmailResponseVariables.LAST_OPEN_DATE,
+        col(EmailResponseVariables.OPENS_TODAY).cast(IntegerType) as EmailResponseVariables.OPENS_TODAY,
+        col(MergeUtils.NEW_ + EmailResponseVariables.CLICKS_TODAY).cast(IntegerType) as EmailResponseVariables.CLICKS_TODAY,
+        outputCsvFormat(col(MergeUtils.NEW_ + EmailResponseVariables.LAST_CLICK_DATE)) as EmailResponseVariables.LAST_CLICK_DATE)
+      .withColumn(EmailResponseVariables.OPENS_TODAY, findOpen(col(EmailResponseVariables.OPENS_TODAY), col(EmailResponseVariables.CLICKS_TODAY))).cache()
+
+    print(joinedDf.first().get(joinedDf.first().fieldIndex("UID") ))
     assert(joinedDf.count() == 15)
 
   }
