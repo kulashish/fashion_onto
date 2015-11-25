0451b0729c9e95b1dc944c8437df0b8d5abf51ac
Rahul Aneja <rahul.aneja@jabong.com>
2015-11-25 12:27:31 +0530

    fixed acart hourly campaign

--- core/scripts/run.pl
+++ core/scripts/run.pl
@@ -352,9 +352,9 @@ if ($component eq "bobAcqFull1") {
     my $command = "$BASE_SPARK_SUBMIT $AMMUNITION $HIVE_JARS $CORE_JAR --component city_wise_data --config $HDFS_CONF/config.json --paramJson $HDFS_CONF/cityWiseData.json";
     $job_exit = run_component($component, $command);
 } elsif ($component eq "acartHourlyFeed") {
-      my $AMMUNITION = "--num-executors 5 --executor-memory 400M";
-      my $command = "$BASE_SPARK_SUBMIT $AMMUNITION $CORE_JAR --component acartHourlyFeed --config $HDFS_CONF/config.json";
-      $job_exit = run_component($component, $command);
+    my $AMMUNITION = "--num-executors 5 --executor-memory 400M";
+    my $command = "$BASE_SPARK_SUBMIT $AMMUNITION $CORE_JAR --component acartHourlyFeed --config $HDFS_CONF/config.json";
+    $job_exit = run_component($component, $command);
   } else {
     print "not a valid component\n";
     $job_exit = -1;
