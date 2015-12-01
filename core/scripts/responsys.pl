c172219364f72163df3e4e947d14527dd5108b92
Pooja Jain <pooja.jain@jabong.com>
2015-12-01 09:49:03 +0530

    added copy to test folder

--- core/scripts/responsys.pl
+++ core/scripts/responsys.pl
@@ -28,6 +28,11 @@ system("hadoop fs -copyFromLocal 53699_SMS_DELIVERED_$date_with_zero" . "_*.txt 
 system("perl /opt/alchemy-core/current/bin/run.pl -t PROD -c dndMerger");
 system("perl /opt/alchemy-core/current/bin/run.pl -t PROD -c smsOptOutMerger");
 
+#copying to test folder for jabongtest run of contactListMobile.
+system("hadoop fs -cp /data/output/responsys/sms_opt_out/full/$date /data/test/output/responsys/sms_opt_out/full/$date");
+system("hadoop fs -cp /data/output/responsys/DND/full/$date /data/test/output/responsys/DND/full/$date");
+system("hadoop fs -cp /data/output/solutionsInfiniti/block_list_numbers/full/$date /data/test/output/solutionsInfiniti/block_list_numbers/full/$date");
+
 system("lftp -c 'set sftp:connect-program \"ssh -a -x -i ./u1.pem\"; connect sftp://jabong_scp:dummy\@files.dc2.responsys.net; mget download/53699_OPEN_$date_with_zero" . "_* ;'");
 # copy data to hdfs
 system("hadoop fs -mkdir -p /data/input/responsys/open/daily/$date/");
@@ -44,3 +49,4 @@ system("hadoop fs -mkdir -p /data/input/responsys/click/daily/$date/");
 system("hadoop fs -copyFromLocal 53699_CLICK_$date_with_zero" . "_*.txt /data/input/responsys/click/daily/$date/53699_CLICK_$date_with_zero" . ".txt");
 
 system("lftp -c 'set sftp:connect-program \"ssh -a -x -i ./u1.pem\"; connect sftp://jabong_scp:dummy\@files.dc2.responsys.net; mget archive/53699_33838_$date_with_zero_yesterday" . "_LIVE_CAMPAIGN.csv.zip ;'");
+
