9f65f8a7fb24ceb3ad63a0dc699c13c4cdcc714f
raghu <raghu@raghu1381>
2015-12-02 12:47:37 +0530

    added Quality for variables files

--- core/src/main/scala/com/jabong/dap/quality/campaign/CampaignQuality.scala
+++ core/src/main/scala/com/jabong/dap/quality/campaign/CampaignQuality.scala
@@ -202,6 +202,9 @@ object CampaignQuality extends Logging {
           } else if (fieldNames.contains(ContactListMobileVars.UID)) {
             countNull = dataFrame.filter(col(ContactListMobileVars.UID).isNull).count()
             countNotNull = dataFrame.filter(col(ContactListMobileVars.UID).isNotNull).count()
+          } else if (fieldNames.contains(CustomerVariables.UID)) {
+            countNull = dataFrame.filter(col(CustomerVariables.UID).isNull).count()
+            countNotNull = dataFrame.filter(col(CustomerVariables.UID).isNotNull).count()
           } else if (fieldNames.contains(CustomerVariables.FK_CUSTOMER)) {
             countNull = dataFrame.filter(col(CustomerVariables.FK_CUSTOMER).isNull || col(CustomerVariables.FK_CUSTOMER).leq(0)).count()
             countNotNull = dataFrame.filter(col(CustomerVariables.FK_CUSTOMER).isNotNull && col(CustomerVariables.FK_CUSTOMER).gt(0)).count()
