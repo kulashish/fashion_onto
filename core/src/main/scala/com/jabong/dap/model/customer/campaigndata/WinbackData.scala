cf07617b96efa941a85c303f3da340f6225fc493
Samatha Shetty <samatha.shetty@jabong.com>
2015-12-03 11:27:47 +0530

    Winback with println

--- core/src/main/scala/com/jabong/dap/model/customer/campaigndata/WinbackData.scala
+++ core/src/main/scala/com/jabong/dap/model/customer/campaigndata/WinbackData.scala
@@ -13,6 +13,8 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types.IntegerType
 import org.apache.spark.sql.functions._
 
+import scala.annotation.elidable
+import scala.annotation.elidable._
 import scala.collection.mutable
 
 /**
@@ -27,9 +29,10 @@ object WinbackData extends DataFeedsModel {
   }
 
   override def readDF(incrDate: String, prevDate: String, paths: String): mutable.HashMap[String, DataFrame] = {
-    dateStr = incrDate
 
     val dateDiffFormat = TimeUtils.changeDateFormat(incrDate, TimeConstants.DATE_FORMAT_FOLDER, TimeConstants.DATE_FORMAT)
+    dateStr = dateDiffFormat
+
     val crmTicketMasterIncr = DataReader.getDataFrame4mOrc(ConfigConstants.INPUT_PATH, DataSets.CRM, DataSets.CRM_TicketMaster, DataSets.FULL_FETCH_MODE, dateDiffFormat)
     println(ConfigConstants.INPUT_PATH + "/" + DataSets.CRM + "/" + DataSets.CRM_TicketMaster)
     val crmTicketDetailsIncr = DataReader.getDataFrame4mOrc(ConfigConstants.INPUT_PATH, DataSets.CRM, DataSets.CRM_TicketDetails, DataSets.DAILY_MODE, incrDate)
@@ -43,18 +46,34 @@ object WinbackData extends DataFeedsModel {
     val dfMap: mutable.HashMap[String, DataFrame] = new mutable.HashMap[String, DataFrame]
 
     dfMap.put("crmTicketMasterIncr", crmTicketMasterIncr)
+    println("Master Data AT read")
+    println(crmTicketMasterIncr.count())
+    println(crmTicketMasterIncr.distinct.count())
     crmTicketMasterIncr.printSchema()
-    println(crmTicketMasterIncr.count)
-    println(crmTicketMasterIncr.distinct.count)
 
     dfMap.put("crmTicketDetailsIncr", crmTicketDetailsIncr)
+    println("Details Data AT read")
     crmTicketDetailsIncr.printSchema()
     println(crmTicketDetailsIncr.count)
     println(crmTicketDetailsIncr.distinct.count)
 
     dfMap.put("crmTicketStatLogIncr", crmTicketStatLogIncr)
+    println("Status Data AT read")
+    crmTicketStatLogIncr.printSchema()
+    println(crmTicketStatLogIncr.count)
+    println(crmTicketStatLogIncr.distinct.count)
+
     dfMap.put("cmrFull", cmrFull)
+    println("Status Data AT read")
+    cmrFull.printSchema()
+    println(cmrFull.count)
+    println(cmrFull.distinct.count)
+
     dfMap.put("salesOrder", days_45Order)
+    println("Status Data AT read")
+    days_45Order.printSchema()
+    println(days_45Order.count)
+    println(days_45Order.distinct.count)
 
     dfMap
   }
@@ -74,6 +93,12 @@ object WinbackData extends DataFeedsModel {
       col(CrmTicketVariables.ISSUE_DESCRIPTION)
     )
 
+    println("Master Data")
+    println(crmTicketMasterIncr.count())
+    println(crmTicketMasterIncr.distinct.count())
+    crmTicketMasterIncr.printSchema()
+
+
     val crmTicketDetailsIncr = dfMap("crmTicketDetailsIncr").select(
       col(CrmTicketVariables.ISSUE_ID),
       col(CrmTicketVariables.TICKET_ID),
@@ -82,16 +107,35 @@ object WinbackData extends DataFeedsModel {
       col(CrmTicketVariables.ORDER_NO)
     )
 
+    println("Details Data")
+    println(crmTicketDetailsIncr.count())
+    println(crmTicketDetailsIncr.distinct.count())
+    crmTicketDetailsIncr.printSchema()
+
     val crmTicketStatLogIncr = dfMap("crmTicketStatLogIncr").select(
       col(CrmTicketVariables.TICKET_ID),
       col(CrmTicketVariables.ADD_DATE),
       col(CrmTicketVariables.EXIT_TICKET_STATUS),
       col(CrmTicketVariables.IN_DT) as CrmTicketVariables.DG_END_DATE
     )
-    val cmrFull = dfMap("cmrFull")
-    val salesOrder = dfMap("salesOrder")
+    println("Status Log Data")
+    println(crmTicketStatLogIncr.count())
+    println(crmTicketStatLogIncr.distinct.count())
+    crmTicketStatLogIncr.printSchema()
 
-    val yesterday = TimeUtils.getDateAfterNDays(-1, TimeConstants.DATE_FORMAT_FOLDER, dateStr)
+    val cmrFull = dfMap("cmrFull")
+    println("CMR Log Data")
+    println(cmrFull.count())
+    println(cmrFull.distinct.count())
+    cmrFull.printSchema()
+
+    val salesOrder = dfMap("salesOrder")
+    println("SalesOrder Log Data")
+    println(salesOrder.count())
+    println(salesOrder.distinct.count())
+    salesOrder.printSchema()
+
+    val yesterday = TimeUtils.getDateAfterNDays(-1, TimeConstants.DATE_FORMAT, dateStr)
 
     //TODO: use constant for the status with  meaningful name
 
@@ -100,7 +144,7 @@ object WinbackData extends DataFeedsModel {
         join(crmTicketStatLogIncr, crmTicketDetailsIncr(CrmTicketVariables.TICKET_ID) === crmTicketStatLogIncr(CrmTicketVariables.TICKET_ID), SQL.INNER).
         join(crmTicketMasterIncr, crmTicketMasterIncr(CrmTicketVariables.ISSUE_ID) === crmTicketDetailsIncr(CrmTicketVariables.ISSUE_ID), SQL.INNER).
         join(cmrFull, crmTicketDetailsIncr(CrmTicketVariables.CUSTOMER_NO) === cmrFull(CustomerVariables.ID_CUSTOMER), SQL.INNER).
-        join(salesOrder, cmrFull(CustomerVariables.ID_CUSTOMER) === salesOrder(SalesOrderVariables.FK_CUSTOMER)).
+        join(salesOrder, cmrFull(CustomerVariables.ID_CUSTOMER) === salesOrder(SalesOrderVariables.FK_CUSTOMER), SQL.LEFT_OUTER).
         where(
           crmTicketStatLogIncr(CrmTicketVariables.ADD_DATE).geq(yesterday).
             and(crmTicketStatLogIncr(CrmTicketVariables.EXIT_TICKET_STATUS).cast(IntegerType) equalTo (21)).
@@ -119,11 +163,21 @@ object WinbackData extends DataFeedsModel {
             crmTicketMasterIncr(CrmTicketVariables.ISSUE_DESCRIPTION) as CrmTicketVariables.ISSUE_DESCRIPTION
           )
 
+    println("Result Log Data")
+    println(result.count())
+    println(result.distinct.count())
+    result.printSchema()
     val incrMap: mutable.HashMap[String, DataFrame] = new mutable.HashMap[String, DataFrame]
 
     incrMap.put("result", result)
 
     incrMap
 
+  }
+
+  @elidable(FINE) def debug(data: DataFrame, name: String) {
+    println("Count of " + name + ":-" + data.count() + "\n")
+    println("show dataframe " + name + ":-" + data.show(10) + "\n")
+    data.printSchema()
   }
 }
