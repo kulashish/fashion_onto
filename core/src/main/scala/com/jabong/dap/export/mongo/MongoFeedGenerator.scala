eeff012befb65de66752859da6b2e11e0e0b883f
Rahul Aneja <rahul.aneja@jabong.com>
2015-12-22 21:21:03 +0530

    added mongo db insert

---
+++ core/src/main/scala/com/jabong/dap/export/mongo/MongoFeedGenerator.scala
@@ -0,0 +1,30 @@
+package com.jabong.dap.export.mongo
+
+import com.jabong.dap.common.OptionUtils
+import com.jabong.dap.common.constants.config.ConfigConstants
+import com.jabong.dap.common.time.{TimeConstants, TimeUtils}
+import com.jabong.dap.data.acq.common.ParamInfo
+import com.jabong.dap.data.read.DataReader
+import com.jabong.dap.data.storage.DataSets
+import grizzled.slf4j.Logging
+import org.apache.spark.sql.SaveMode
+
+/**
+ * Created by rahul on 22/12/15.
+ */
+object MongoFeedGenerator  extends Logging {
+
+  /**
+   * Starting point of mongo feed generation
+   * @param params
+   */
+  def   start(params: ParamInfo) {
+    logger.info("mongo feed generation process started")
+    val executeDate = OptionUtils.getOptValue(params.incrDate, TimeUtils.getDateAfterNDays(-1, TimeConstants.DATE_FORMAT_FOLDER))
+    val options = Map("host" -> " 172.16.90.53:27017", "user"->"reco", "pass" ->"xD8b6jG72", "database" -> "reco", "collection" -> "poc")
+    val brickMvpSearchRecommendations = DataReader.getDataFrame(ConfigConstants.READ_OUTPUT_PATH,DataSets.RECOMMENDATIONS,DataSets.BRICK_MVP_SEARCH_RECOMMENDATIONS,DataSets.DAILY_MODE,executeDate)
+    // df4.write.format("org.apache.spark.sql.cassandra").mode(SaveMode.Overwrite).options(Map( "table" -> "poc", "keyspace" -> "reco")).save()
+    brickMvpSearchRecommendations.write.format("com.stratio.datasource.mongodb").mode(SaveMode.Overwrite).options(options).save()
+
+  }
+}
